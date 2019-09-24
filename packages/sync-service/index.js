5ff9d4289ffc08c2f16451f49cf2426915e0f836
mwcz <mwc@clayto.com>
2019-09-24 16:45:32 -0400

    chore: add version number to startup logs

--- packages/sync-service/index.js
+++ packages/sync-service/index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const config = require("./config");
 const routes = require("./routes/routes");
 const Autosync = require("./background/autosync");
+const package = require("./package.json");
 
 const app = express();
 const autosync = new Autosync();
@@ -18,7 +19,7 @@ console.log(` ____  ____   __   __ _  ____  _  _
 \\___ \\ ) __//    \\/    / ) D ( )  (
 (____/(__)  \\_/\\_/\\_)__)(____/(_/\\_)
 `);
-console.log(`Running with configuration:\n`);
+console.log(`Starting SPANDX version ${package.version} with configuration:\n`);
 console.log(config.toString());
 console.log();
 console.log(`Listening on http://${config.get("host")}:${config.get("port")}`);
