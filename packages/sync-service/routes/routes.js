6d407d6655831016c66063b5db669474408c1865
Michael Clayton <mclayton@redhat.com>
2019-09-24 16:14:48 -0400

    feat: support cross-origin HTTP requests (#15)

--- packages/sync-service/routes/routes.js
+++ packages/sync-service/routes/routes.js
@@ -3,22 +3,31 @@ const list = require("./list/list");
 const fs = require("fs");
 const path = require("path");
 const forceSyncAll = require("./forceSyncAll/forceSyncAll");
+const corsMiddleware = require("cors");
+
+const cors = corsMiddleware({
+  origin: true,
+  credentials: true
+});
 
 function register(app) {
+  // Add `cors` to each request that needs CORS headers. See existing
+  // cors-enabled routes for examples.
+
   app.route("/").get((req, res) => res.redirect("/deploy"));
 
   app
     .route("/deploy")
-    .post(...deploy())
+    .post(cors, ...deploy())
     .get((req, res) => {
       fs.readFile(path.resolve(__dirname, "..", "index.html"), (err, data) => {
         res.send(data.toString());
       });
     });
 
-  app.route("/list").get(list());
+  app.route("/list").get(cors, list());
 
-  app.post("/autosync/forceSyncAll", forceSyncAll());
+  app.post("/autosync/forceSyncAll", cors, forceSyncAll());
 }
 
 module.exports = { register };
