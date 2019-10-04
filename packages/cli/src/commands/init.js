d81c9e8baa3e082c941b7bda557085dfc560eaf1
mwcz <mwc@clayto.com>
2019-10-04 13:36:19 -0400

    fix init flags

--- packages/cli/src/commands/init.js
+++ packages/cli/src/commands/init.js
@@ -1,24 +1,21 @@
 const {Command, flags} = require('@oclif/command')
 
 class InitCommand extends Command {
-  static get flags() {
-    return {
-      name: flags.string({char: 'n', description: 'the name of your app', default: 'asdf'}),
-      path: flags.string({char: 'p', description: 'the URL path'}),
-      single: flags.boolean({char: 's', description: 'route all non-asset requests to index.html'}),
-    }
-  }
-
-  static get description() {
-    return `Initialize a SPAship config file for your app.
-Without arguments, init will ask you a few questions and generate a spaship.yaml config file.  The answers can also be passed in as CLI options.
-`
-  }
-
   async run() {
     const r = this.parse(InitCommand)
+    this.log(r.flags);
     this.log(`hello ${r.flags.name} from /home/mclayton/projects/@spaship/cli/src/commands/init.js`)
   }
+}
+
+InitCommand.description = `Initialize a SPAship config file for your app.
+Without arguments, init will ask you a few questions and generate a spaship.yaml config file.  The answers can also be passed in as CLI options.
+`
+
+InitCommand.flags = {
+  name: flags.string({char: 'n', description: 'the name of your app' }),
+  path: flags.string({char: 'p', description: 'the URL path'}),
+  single: flags.boolean({char: 's', description: 'route all non-asset requests to index.html'}),
 }
 
 module.exports = InitCommand
