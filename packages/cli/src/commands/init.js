d7367e9cd647f23222624225c6b9093380c02cb0
mwcz <mwc@clayto.com>
2019-10-04 13:42:03 -0400

    add prettier

--- packages/cli/src/commands/init.js
+++ packages/cli/src/commands/init.js
@@ -1,21 +1,26 @@
-const {Command, flags} = require('@oclif/command')
+const { Command, flags } = require("@oclif/command");
 
 class InitCommand extends Command {
   async run() {
-    const r = this.parse(InitCommand)
+    const r = this.parse(InitCommand);
     this.log(r.flags);
-    this.log(`hello ${r.flags.name} from /home/mclayton/projects/@spaship/cli/src/commands/init.js`)
+    this.log(
+      `hello ${r.flags.name} from /home/mclayton/projects/@spaship/cli/src/commands/init.js`
+    );
   }
 }
 
 InitCommand.description = `Initialize a SPAship config file for your app.
 Without arguments, init will ask you a few questions and generate a spaship.yaml config file.  The answers can also be passed in as CLI options.
-`
+`;
 
 InitCommand.flags = {
-  name: flags.string({char: 'n', description: 'the name of your app' }),
-  path: flags.string({char: 'p', description: 'the URL path'}),
-  single: flags.boolean({char: 's', description: 'route all non-asset requests to index.html'}),
-}
+  name: flags.string({ char: "n", description: "the name of your app" }),
+  path: flags.string({ char: "p", description: "the URL path" }),
+  single: flags.boolean({
+    char: "s",
+    description: "route all non-asset requests to index.html"
+  })
+};
 
-module.exports = InitCommand
+module.exports = InitCommand;
