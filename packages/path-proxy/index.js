ff1418c01d5a996fa87c73d5c300379b91e363c0
Jared Sprague <jared@caramelcode.com>
2019-10-07 17:31:30 -0400

    WIP url routing

--- packages/path-proxy/index.js
+++ packages/path-proxy/index.js
@@ -1,37 +1,94 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
+const fsp = require('fs').promises;
 
-const pathToFlat = function(req) {
+let spaPaths = [];
+
+/**
+ * Converts a flattened path to a URL path
+ * @param flatPath
+ */
+function flatToUri(flatPath) {
+    let urlPath = flatPath;
+
+    // Replace _ with /
+    urlPath = urlPath.replace(/_/g, "/");
+
+    // Replace UNDRSCR token with _
+    urlPath = urlPath.replace(/UNDRSCR/g, "_");
+
+    // Prepend /
+    urlPath = "/" + urlPath;
+
+    console.log('[flatToUrl] urlPath:', urlPath);
+
+    return urlPath;
+}
+
+async function getDirectoryNames() {
+    spaPaths = await fsp.readdir('/var/www/spaship');
+    spaPaths.sort((a, b) => b.length - a.length);
+    // return spaPaths;
+}
+
+getDirectoryNames();
+
+const uriToFlat = function(req) {
     let path = req.url;
     let flatPath;
+    let match;
+    let spaUri;
+    let matchedFlatPath;
+
+    console.log('[router] incoming path:', path);
+
+    // See if this URL is hosted by SPAship
+    for (let spaPath of spaPaths) {
+        spaUri = flatToUri(spaPath);
+        let regEx = new RegExp('^' + spaUri + '(\\/+.*)?');
+        console.log('regEx:', regEx);
+
+        match = path.match(regEx);
+        if (match) {
+            matchedFlatPath = spaPath;
+            break;  // found a match
+        }
+    }
+
+    if (match) {
+        console.log('This path is hosted by spaship: ', path, ' spaPath: ', spaUri, 'flatPath: ', matchedFlatPath);
+    }
+    else {
+        console.log('This path is not hosted by spaship: ', path);
+        return 'https://access.redhat.com:443';
+    }
 
     // handle root path
     if (path === "/") {
         flatPath = "root"
     }
     else {
-        // Convert path to flat path
-
-        // Trim off leading /
-        path = path.substr(1);
-
-        // Escape underscores with double underscore
-        path = path.replace(/_/g, "__");
-
-        // Convert normal path to flat path by replacing / with _
-        flatPath = path.replace(/\//g, "_");
+        let extraStuff = "";
+        if (match[1]) {
+            extraStuff = match[1];
+        }
+        flatPath = matchedFlatPath + extraStuff;
     }
 
     // Now proxy to flat path
-    req.url = '/';
-    return 'http://localhost:8008/' + flatPath;
+    req.url = flatPath;
+    let route = 'http://localhost:8008/';
+    console.log("Routing to: ", route + req.url);
+    return route;
 };
 
 // proxy middleware options
 let options = {
-    target: 'http://localhost:8008', // target host
-    changeOrigin: false,
-    router: pathToFlat
+    // target: 'http://localhost:8008', // target host
+    target: 'https://access.redhat.com:443', // target host
+    changeOrigin: true,
+    router: uriToFlat,
+    logLevel: 'debug'
 };
 
 // create the proxy (without context)
