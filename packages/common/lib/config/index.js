454b72660b9c87c036bf483a50355735f31f8f99
mwcz <mwc@clayto.com>
2019-10-09 13:17:37 -0400

    make deploykey generator available

--- packages/common/lib/config/index.js
+++ packages/common/lib/config/index.js
@@ -2,5 +2,6 @@ const read = require("./read");
 const validate = require("./validate");
 const write = require("./write");
 const append = require("./append");
+const deploykey = require("./deploykey");
 
-module.exports = { read, validate, write, append };
+module.exports = { read, validate, write, append, deploykey };
