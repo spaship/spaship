3339385bcd4a28418936a21fa09bd92039a04b31
mwcz <mwc@clayto.com>
2019-10-09 22:27:11 -0400

    add spaship deploy command

--- packages/cli/src/commands/deploy.js
+++ packages/cli/src/commands/deploy.js
@@ -1,22 +1,42 @@
+const path = require("path");
 const { Command, flags } = require("@oclif/command");
+const execa = require("execa");
 
 class DeployCommand extends Command {
   async run() {
-    const { flags } = this.parse(DeployCommand);
-    const name = flags.name || "world";
-    this.log(
-      `hello ${name} from /home/mclayton/projects/spaship/cli/src/commands/deploy.js`
-    );
+    const { args, flags } = this.parse(DeployCommand);
+    this.log(`Deploying SPA...`);
+    try {
+      const { stdout, stderr } = await execa.command(
+        `curl http://spaship.usersys.redhat.com:8008/deploy -X POST -F upload=@${args.archive} -F ref=${flags.ref} -v`
+      );
+      this.log(stdout);
+    } catch (e) {
+      this.error(e);
+    }
   }
 }
 
-DeployCommand.description = `Describe the command here
-...
-Extra documentation goes here
+DeployCommand.description = `deploy to a SPAship host
+Send an archive containing a SPA to a SPAship host for deployment.  Supports .tar.gz/.tgz, .zip, and .tar.bz2.
 `;
 
+DeployCommand.args = [
+  {
+    name: "archive",
+    required: true,
+    description: "SPA archive file"
+  }
+];
+
 DeployCommand.flags = {
-  name: flags.string({ char: "n", description: "name to print" })
+  ref: flags.string({
+    char: "r",
+    description:
+      "a version tag, commit hash, or branch to identify this release",
+    required: false,
+    default: "undefined"
+  })
 };
 
 module.exports = DeployCommand;
