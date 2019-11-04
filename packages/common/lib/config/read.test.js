176af000c28ed83644b4d9d73042486d2000c679
mwcz <mwc@clayto.com>
2019-11-04 10:44:07 -0500

    test: refactor config/read test to use mockfs

--- packages/common/lib/config/read.test.js
+++ packages/common/lib/config/read.test.js
@@ -1,15 +1,24 @@
 const fs = require("fs");
 const read = require("./read");
+const mockfs = require("mock-fs");
 
 describe("common.config.read", () => {
+  beforeEach(() => {
+    mockfs({
+      "spaship.yaml": "name: Foo\npath: /foo\nref: v1.0.1\nsingle: true\ndeploykey: sehvgqrnyre"
+    });
+  });
+  afterEach(() => {
+    mockfs.restore();
+  });
   test("should read configuration data from a spaship.yaml file", async () => {
     // mock readFile
-    fs.promises.readFile = jest.fn();
+    // fs.promises.readFile = jest.fn();
 
-    const mockFileContent = "name: Foo\npath: /foo";
-    fs.promises.readFile.mockResolvedValue(mockFileContent);
+    // const mockFileContent = "name: Foo\npath: /foo";
+    // fs.promises.readFile.mockResolvedValue(mockFileContent);
 
-    const data = await read("./foo");
-    expect(data).toEqual({ name: "Foo", path: "/foo" });
+    const data = await read("./spaship.yaml");
+    expect(data).toEqual({ name: "Foo", path: "/foo", deploykey: "sehvgqrnyre", single: true, ref: "v1.0.1" });
   });
 });
