a9daeba4c038d9abd5c20c21d944036556b4bc7a
mwcz <mwc@clayto.com>
2020-01-27 11:18:37 -0500

    clean up common.config.read test mocks

--- packages/common/lib/config/read.test.js
+++ packages/common/lib/config/read.test.js
@@ -1,25 +1,18 @@
 const fs = require("fs");
 const read = require("./read");
-const mockfs = require("mock-fs");
 
 describe("common.config.read", () => {
-  // beforeAll(() => {
-  //   mockfs({
-  //     "spaship.yaml": "name: Foo\npath: /foo\nref: v1.0.1\nsingle: true\ndeploykey: sehvgqrnyre"
-  //   });
-  // });
+  beforeAll(() => {
+    fs.promises.readFile = jest.fn();
+  });
   afterAll(() => {
-    // mockfs.restore();
+    fs.promises.readFile.mockRestore();
   });
   test("should read configuration data from a spaship.yaml file", async () => {
-    // mock readFile
-    fs.promises.readFile = jest.fn();
     const mockFileContent = "name: Foo\npath: /foo\nref: v1.0.1\nsingle: true\ndeploykey: sehvgqrnyre";
     fs.promises.readFile.mockResolvedValue(mockFileContent);
 
     const data = await read("./spaship.yaml");
     expect(data).toEqual({ name: "Foo", path: "/foo", deploykey: "sehvgqrnyre", single: true, ref: "v1.0.1" });
-
-    fs.promises.readFile.mockRestore();
   });
 });
