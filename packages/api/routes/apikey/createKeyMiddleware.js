7632cce5f69b13c442a9524a3969460ea086c7bf
Sayak Sarkar <sayak.bugsmith@gmail.com>
2020-03-13 19:02:25 +0530

    feat(apiKeys): Added checks for request body.

--- packages/api/routes/apikey/createKeyMiddleware.js
+++ packages/api/routes/apikey/createKeyMiddleware.js
@@ -4,9 +4,14 @@ const db_apikey = require("../../lib/db.apikey");
 module.exports = function createKeyMiddleware() {
   return async (req, res, next) => {
     const apikey = await db_apikey.attach();
-    apikey.createKey(req.body.user).then(doc => {
-      res.send(doc);
-      next();
-    });
+    const user = req.body.user ? req.body.user.trim() : req.body.user;
+    const doc = user
+      ? await apikey.createKey(req.body.user)
+      : {
+          error: "Invalid Parameter",
+          message: "Username Missing: Username cannot be empty. It is needed to create and assign a new API Key."
+        };
+    res.send(doc);
+    next();
   };
 };
