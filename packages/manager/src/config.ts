7e1615f32f2b4d06516cc0f7f50ca77d6eda58e2
Kun Yan <kyan@redhat.com>
2020-03-24 20:56:05 +0800

    fix(manager): react test error when config changed

--- packages/manager/src/config.ts
+++ packages/manager/src/config.ts
@@ -3,19 +3,19 @@ export interface IEnvironment {
   api: string;
   domain: string;
 }
+
+export interface IConfig {
+  environments: IEnvironment[];
+  keycloak: {
+    url: string;
+    realm: string;
+    clientId: string;
+  };
+}
 declare global {
   interface Window {
-    SPAship: {
-      environments: IEnvironment[];
-      keycloak: {
-        url: string;
-        realm: string;
-        clientId: string;
-      };
-    };
+    SPAship: IConfig;
   }
 }
 
-const config = window.SPAship;
-
-export default config;
+export default window.SPAship;
