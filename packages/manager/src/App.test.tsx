7d27d7270a02fefdce90563c48a3227ed6aa8ee9
Kun Yan <kyan@redhat.com>
2020-03-25 09:19:25 +0800

    fix(manager): can not found keycloak of undefined

--- packages/manager/src/App.test.tsx
+++ packages/manager/src/App.test.tsx
@@ -2,15 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-window.SPAship = {
-  environments: [],
-  keycloak: {
-    url: "https://xxxx.com",
-    realm: "xxxx",
-    clientId: "xxxx",
-  },
-};
-
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
