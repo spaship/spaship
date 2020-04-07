cb7404e62951e79186074f272cb94d4c15d0a45c
Kun Yan <kyan@redhat.com>
2020-04-07 14:22:32 +0800

    fix no res.send error. add default response for sync

--- packages/sync/index.js
+++ packages/sync/index.js
@@ -6,12 +6,18 @@ const AutoSync = require("./lib/autoSync");
 const autoSync = new AutoSync();
 const port = config.get("port");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/force" && req.method === "GET") {
-    autoSync.forceSyncAll();
-    res.send("Forcing all autoSync targets to sync now");
+const requestListener = async function (req, res) {
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200);
+    res.end("I'm alive");
+  } else if (req.url === "/force" && req.method === "GET") {
+    await autoSync.forceSyncAll();
+    res.writeHead(200);
+    res.end("Forcing all autoSync targets to sync now");
   }
-});
+};
+
+const server = http.createServer(requestListener);
 
 server.listen(port, () => {
   log.info(`Server is listening on port ${port} 🚀`);
