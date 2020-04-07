fece3e98e6bab8f8d02ffce0d52de4ccc11d8e18
Kun Yan <kyan@redhat.com>
2020-04-07 15:13:01 +0800

    move new autoSync to different place to make it work with /force

--- packages/sync/index.js
+++ packages/sync/index.js
@@ -3,11 +3,11 @@ const { log } = require("@spaship/common/lib/logging/pino");
 const config = require("./config");
 const AutoSync = require("./lib/autoSync");
 
-const autoSync = new AutoSync();
 const port = config.get("port");
 
 const requestListener = function (req, res) {
   if (req.url === "/force" && req.method === "GET") {
+    const autoSync = new AutoSync();
     autoSync.forceSyncAll();
     res.writeHead(200);
     res.end("Forcing all autoSync targets to sync now");
@@ -25,6 +25,7 @@ server.listen(port, () => {
 });
 
 if (config.get("autosync:enabled")) {
+  const autoSync = new AutoSync();
   autoSync.start();
   if (config.get("autosync:onstart")) {
     autoSync.forceSyncAll();
