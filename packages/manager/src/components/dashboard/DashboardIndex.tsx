ada83138ed8f1be9695ff7cb3867f708da426418
Kun Yan <kyan@redhat.com>
2020-04-14 16:17:12 +0800

    fix typo

--- packages/manager/src/components/dashboard/DashboardIndex.tsx
+++ packages/manager/src/components/dashboard/DashboardIndex.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import Page from "../../layout/Page";
 export default () => {
-  return <Page title="Dashboard">Comming Soon !</Page>;
+  return <Page title="Dashboard">Coming Soon !</Page>;
 };
