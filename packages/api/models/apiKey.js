9f070cc30f5863d8dfce79df55792547d8f16ebd
Kun Yan <kyan@redhat.com>
2020-04-14 15:41:46 +0800

    fix(api): apiKey label should uniqu by userId

--- packages/api/models/apiKey.js
+++ packages/api/models/apiKey.js
@@ -6,8 +6,6 @@ const APIKeySchema = new Schema(
     label: {
       type: String,
       required: true,
-      index: true,
-      unique: true,
     },
     shortKey: {
       type: String,
@@ -25,5 +23,7 @@ const APIKeySchema = new Schema(
     timestamps: true,
   }
 );
+
+APIKeySchema.index({ label: 1, userId: 1 }, { unique: true });
 
 module.exports = mongoose.model("APIKey", APIKeySchema);
