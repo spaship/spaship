794b31a1750d7ab2d518c830d1fceff4437713cb
tanyutu <tanweitanya@gmail.com>
2020-04-16 13:54:48 +0800

    Add E2E test  (#305)
    * Add E2E test for api and ui
    
    * modify the readme
    
    * modify the e2e according to the comment -v1
    
    * update the e2e according to the comment -v2
    
    * udpate the code according to the comment -v3
    
    Co-authored-by: wetan <wetan@redhat.com>

---
+++ e2e/manager/README.md
@@ -0,0 +1,9 @@
+It's e2e test for spaship manager
+
+How to test:
+
+$ npm install
+$ npm test -- -e {hostname} -u {login_username} -p {login_passw} -a {application_path}
+
+Check the test result in /cypress/reports
+Check the test vedio in /cypress/vedio
---
+++ e2e/manager/cypress/integration/spa_spec.js
@@ -0,0 +1,71 @@
+describe("E2E happy test for SPAship manager", function () {
+  var NAME;
+  // SSO login
+  before(() => {
+    NAME = Cypress.moment().format("x");
+  });
+  // before each test will do sso login
+  beforeEach(function () {
+    cy.visit(Cypress.env("host"));
+    cy.get("#username").type(Cypress.env("username"));
+    cy.get("#password").type(Cypress.env("passw"));
+    cy.get("#submit").click();
+    cy.get("#root").should("exist");
+    cy.log("SSO login successfully");
+  });
+  // after each test will do sso logout
+  afterEach(function () {
+    cy.get("#userInfo").click();
+    cy.get("#logout-button").click();
+  });
+
+  // test of new application creation, not recommand to use it
+  // it('New a application and list it', function(){
+  // cy.get('#add-application-button').click()
+  // var test_name = 'name'+ NAME
+  // var test_path = '/test'+'/'+NAME
+  // var ref = 'v_qa_1.0.0'
+  // cy.get('#name').type(test_name)
+  // cy.get('select').select('QA')
+  // cy.get('#path').type(test_path)
+  // cy.get('#ref').type(ref)
+  // cy.get('#upload').attachFile('test.tar')
+  // cy.contains('Submit').click()
+  // cy.get('#search-application-text').type(test_path)
+  // cy.contains(test_name).should('exist')
+  // })
+
+  it("application list", function () {
+    cy.get("#search-application-text").type(Cypress.env("app_path"));
+    cy.contains(Cypress.env("app_path")).click();
+  });
+
+  //create api key
+  it("API key generation -new", function () {
+    var env_list = ["QA"];
+    var key_name = "key" + NAME;
+    cy.get('a[href*="authentication"]').click();
+    cy.contains("Create API Key").click();
+    cy.get("#api-key-label").type(key_name);
+    cy.wrap(env_list).each((index) => {
+      cy.get("#env-" + index).click();
+    });
+    cy.contains("Create API key").click();
+    cy.get(".pf-c-input-group__text").each(($div, i, $divs) => {
+      expect($divs).to.contain(env_list[i]);
+    });
+    cy.get('button[aria-label="Close"]').click();
+  });
+
+  it("API key delete", function () {
+    var key_name = "key" + NAME;
+    cy.get('a[href*="authentication"]').click();
+    cy.contains(key_name).should("exist");
+    cy.contains(key_name).siblings('td[data-label="Scope"]').children("button").click();
+    cy.wait(10);
+    cy.contains("Revoke").click({ force: true });
+    cy.contains("Confirm").click();
+    cy.reload();
+    cy.contains(key_name).should("not.exist");
+  });
+});
