ac0ec383f435afbb1eb0b36ddfb68836bfec272e
Jared Sprague <Jared-Sprague@users.noreply.github.com>
2020-04-19 13:08:27 -0400

    fixes #329 - add root path spa support to router (#349)
    * fixes #329 - add root path spa support to router
    
    * Fixes #329 - Fixed jest tests

--- packages/router/jest.config.js
+++ packages/router/jest.config.js
@@ -1,4 +1,9 @@
+process.env.SPASHIP_WEBROOT = "/var/www/spaship";
+process.env.SPASHIP_ROUTER_PORT = "8081";
+process.env.SPASHIP_TARGET = "http://localhost:3333";
+process.env.SPASHIP_LOG_LEVEL = "debug";
+
 module.exports = {
   projects: ["<rootDir>"],
-  collectCoverage: true
+  collectCoverage: true,
 };
