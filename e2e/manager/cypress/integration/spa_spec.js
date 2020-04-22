c051d45f3f94db43e9047f5198a7459ec91c890a
tanyutu <tanweitanya@gmail.com>
2020-04-22 12:37:46 +0800

    test(e2e): Add e2e test for root path (#376)
    * Add E2E test for api and ui
    
    * modify the readme
    
    * modify the e2e according to the comment -v1
    
    * update the e2e according to the comment -v2
    
    * udpate the code according to the comment -v3
    
    * add the new e2e for root path
    
    * modify readme
    
    * modify the code according to the review comment -v1
    
    * Update e2e/manager/cypress/fixtures/test/index.html
    
    Co-Authored-By: Kun Yan <kyan@redhat.com>
    
    * Update e2e/api/test/index.html
    
    Co-Authored-By: Kun Yan <kyan@redhat.com>
    
    * resolve conflict
    
    Co-authored-by: wetan <wetan@redhat.com>
    Co-authored-by: Kun Yan <kyan@redhat.com>

--- e2e/manager/cypress/integration/spa_spec.js
+++ e2e/manager/cypress/integration/spa_spec.js
@@ -1,5 +1,5 @@
 describe("E2E happy test for SPAship manager", function () {
-  var NAME;
+  let NAME;
   // SSO login
   before(() => {
     NAME = Cypress.moment().format("x");
@@ -13,37 +13,16 @@ describe("E2E happy test for SPAship manager", function () {
     cy.get("#root").should("exist");
     cy.log("SSO login successfully");
   });
+
   // after each test will do sso logout
-  afterEach(function () {
-    cy.get("#userInfo").click();
-    cy.get("#logout-button").click();
-  });
+  // afterEach(function () {
+  // cy.get("#userInfo").click();
+  // cy.get("#logout-button").click();
+  // });
 
-  // test of new application creation, not recommand to use it
-  // it('New a application and list it', function(){
-  // cy.get('#add-application-button').click()
-  // var test_name = 'name'+ NAME
-  // var test_path = '/test'+'/'+NAME
-  // var ref = 'v_qa_1.0.0'
-  // cy.get('#name').type(test_name)
-  // cy.get('select').select('QA')
-  // cy.get('#path').type(test_path)
-  // cy.get('#ref').type(ref)
-  // cy.get('#upload').attachFile('test.tar')
-  // cy.contains('Submit').click()
-  // cy.get('#search-application-text').type(test_path)
-  // cy.contains(test_name).should('exist')
-  // })
-
-  it("application list", function () {
-    cy.get("#search-application-text").type(Cypress.env("app_path"));
-    cy.contains(Cypress.env("app_path")).click();
-  });
-
-  //create api key
   it("API key generation -new", function () {
-    var env_list = ["QA"];
-    var key_name = "key" + NAME;
+    const env_list = ["QA"];
+    const key_name = "key" + NAME;
     cy.get('a[href*="authentication"]').click();
     cy.contains("Create API Key").click();
     cy.get("#api-key-label").type(key_name);
@@ -58,14 +37,50 @@ describe("E2E happy test for SPAship manager", function () {
   });
 
   it("API key delete", function () {
-    var key_name = "key" + NAME;
+    const key_name = "key" + NAME;
     cy.get('a[href*="authentication"]').click();
     cy.contains(key_name).should("exist");
     cy.contains(key_name).siblings('td[data-label="Scope"]').children("button").click();
     cy.wait(10);
-    cy.contains("Revoke").click({ force: true });
+    cy.contains("Delete").click({ force: true });
     cy.contains("Confirm").click();
     cy.reload();
     cy.contains(key_name).should("not.exist");
+  });
+
+  it("new root application and router to display", function () {
+    const deploy_env = "Dev";
+    cy.get("#add-application-button").click();
+    var test_name = "name" + NAME;
+    var ref = "v_qa_1.0.0";
+    cy.get("#name").type(test_name);
+    cy.get("select").select(deploy_env);
+    cy.get("#site-host").invoke("text").as("page1value");
+    cy.get("#ref").type(ref);
+    cy.get("#upload").attachFile("test.tar");
+    cy.contains("Submit").click();
+    cy.get("@page1value").then((value) => {
+      cy.visit(value);
+      cy.contains("This is test").should("exist");
+    });
+  });
+
+  it("New a application and list it", function () {
+    var deploy_env = "Dev";
+    cy.get("#add-application-button").click();
+    const test_name = "name" + NAME;
+    const test_path = "/test" + "/" + NAME;
+    const ref = "v_qa_1.0.0";
+    cy.get("#name").type(test_name);
+    cy.get("select").select(deploy_env);
+    cy.get("#site-host").invoke("text").as("page1value");
+    cy.get("#path").type(test_path);
+    cy.get("#ref").type(ref);
+    cy.get("#upload").attachFile("test.tar");
+    cy.contains("Submit").click();
+    cy.get("#search-application-text").type(test_path);
+    cy.contains(test_name).should("exist");
+    cy.contains(test_name).click();
+    cy.get("h2").should("contain", test_name);
   });
 });
