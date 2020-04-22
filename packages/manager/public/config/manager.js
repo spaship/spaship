f5ecd6590342f6a17d7d96c1660ded025e0a104f
Kun Yan <kyan@redhat.com>
2020-04-22 18:03:18 +0800

    fix(manager): show path error in environment details

--- packages/manager/public/config/manager.js
+++ packages/manager/public/config/manager.js
@@ -3,22 +3,22 @@ window.SPAship = {
     {
       name: "Dev",
       api: "https://spaship-api-cpops-dev.ext.us-west.dc.preprod.paas.redhat.com/api/v1",
-      domain: "https://access.dev.redhat.com",
+      domain: "https://spaship-router-cpops-dev.ext.us-west.dc.preprod.paas.redhat.com",
     },
     {
       name: "QA",
       api: "https://spaship-api-cpops-qa.ext.us-west.dc.preprod.paas.redhat.com/api/v1",
-      domain: "https://access.qa.redhat.com",
+      domain: "https://spaship-router-cpops-qa.ext.us-west.dc.preprod.paas.redhat.com",
     },
     {
       name: "Stage",
       api: "https://spaship-api-cpops-stage.ext.us-west.dc.preprod.paas.redhat.com/api/v1",
-      domain: "https://access.stage.redhat.com",
+      domain: "https://spaship-router-cpops-stage.ext.us-west.dc.preprod.paas.redhat.com",
     },
     {
       name: "Prod",
-      api: "https://spaship-api-cpops-prod.ext.us-west.dc.prod.paas.redhat.com/api/v1",
-      domain: "https://access.redhat.com",
+      api: "https://spaship-api-cpops.ext.us-west.dc.prod.paas.redhat.com/api/v1",
+      domain: "https://spaship-router-cpops.ext.us-west.dc.prod.paas.redhat.com",
     },
   ],
   keycloak: {
