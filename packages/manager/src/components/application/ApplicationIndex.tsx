b83454d08310557669ac3e5a060a63b0a08b2e0c
Sayak Sarkar <sayak.bugsmith@gmail.com>
2020-06-01 12:59:08 +0530

    feat(#495): Removed 'Purge Cache' button from the Manager app's Application Details page.

--- packages/manager/src/components/application/ApplicationIndex.tsx
+++ packages/manager/src/components/application/ApplicationIndex.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocation, useParams } from "react-router-dom";
-import { Button, Nav, NavList, NavItem, NavVariants, Tooltip } from "@patternfly/react-core";
+import { Nav, NavList, NavItem, NavVariants } from "@patternfly/react-core";
 import { StyleSheet, css } from "@patternfly/react-styles";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Page from "../../layout/Page";
@@ -17,19 +17,8 @@ export default () => {
   const location = useLocation();
   const { applicationName } = useParams();
 
-  const titleToolbar = (
-    <Tooltip content={<div>This feature would be coming soon</div>}>
-      <div>
-        {" "}
-        <Button title="Coming soon" isDisabled>
-          Purge Cache
-        </Button>
-      </div>
-    </Tooltip>
-  );
-
   return (
-    <Page title={applicationName || ""} titleToolbar={titleToolbar}>
+    <Page title={applicationName || ""}>
       <Nav onSelect={() => {}}>
         <NavList variant={NavVariants.tertiary} className={css(styles.tertiary)}>
           <NavItem isActive={location.pathname.endsWith("details")}>Details</NavItem>
