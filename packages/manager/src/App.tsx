dc607901d879a636747805d8efc952ff79e6df40
mwcz <mwc@clayto.com>
2020-09-17 11:15:09 -0400

    in progress debugging of SSO loop

--- packages/manager/src/App.tsx
+++ packages/manager/src/App.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { KeycloakProvider } from "@react-keycloak/web";
 import { keycloak } from "./keycloak";
+import Keycloak from "keycloak-js";
 import AppRouter from "./Routes";
+
+function kcLog(event: any, error: Keycloak.KeycloakError | undefined) {
+  console.log("KeycloakProvider ", event, keycloak.authenticated, error);
+}
 
 // Wrap everything inside KeycloakProvider
 export default () => {
   return (
     <KeycloakProvider
       keycloak={keycloak}
-      initConfig={{ onLoad: "login-required" }}
+      // initConfig={{ onLoad: "login-required" }}
       onTokens={(...tokens) => console.log("KeycloakProvider onToken:", tokens)}
-      onEvent={(event, error) => console.log("KeycloakProvider onEvent:", event, error)}
+      onEvent={(event, error) => kcLog(event, error)}
     >
       <AppRouter />
     </KeycloakProvider>
