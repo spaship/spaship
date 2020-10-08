3320229acdbc4ae60fc8399d6ae3166c022dc819
mwcz <mwc@clayto.com>
2020-10-08 11:55:34 -0400

    in router's tests, replace mock-fs with memfs

--- packages/router/index.test.js
+++ packages/router/index.test.js
@@ -1,65 +1,42 @@
 const axios = require("axios");
-const mockfs = require("mock-fs");
 const pathProxy = require("./index");
+const fs = require("fs");
+const vol = require("memfs").vol;
+
+jest.mock("fs");
 
 describe("router", () => {
   beforeEach(() => {
-    // Configured using environment variables in ./jest.config.js
-    mockfs({
-      "/var/www/spaship": {
-        foo: {
-          "spaship.yaml": "name: Foo\npath: /foo\nref: v1.0.1\nsingle: true\ndeploykey: sehvgqrnyre",
-          "index.html": "I AM FOO",
-        },
-        ROOTSPA: {
-          "spaship.yaml": "name: Root Spa\npath: /\nref: v1.0.1\nsingle: true\ndeploykey: dc34ldv3sd",
-          "index.html": "I AM ROOTSPA",
-        },
-      },
+    vol.reset();
+    vol.fromJSON({
+      "/var/www/spaship/foo/spaship.yaml": "name: Foo\npath: /foo\nref: v1.0.1\nsingle: true\ndeploykey: sehvgqrnyre",
+      "/var/www/spaship/foo/index.html": "I AM FOO",
+      "/var/www/spaship/ROOTSPA/spaship.yaml":
+        "name: Root Spa\npath: /\nref: v1.0.1\nsingle: true\ndeploykey: dc34ldv3sd",
+      "/var/www/spaship/ROOTSPA/index.html": "I AM ROOTSPA",
     });
   });
-  afterEach(() => {
-    mockfs.restore();
-  });
-  test("should respond to requests", async () => {
-    await pathProxy.start();
-    let response;
-    try {
-      response = await axios.get("http://localhost:8081/foo/");
-    } catch (e) {
-      // errors are fine; this test is only looking for a response
-      response = e.response;
-    }
-    expect(response.status).toBeGreaterThan(1);
-    expect(response.status).toBeLessThan(599);
-    await pathProxy.stop();
-  });
 
-  test("should respond to requests with duplicate slash", async () => {
-    await pathProxy.start();
-    let response;
-    try {
-      response = await axios.get("http://localhost:8081//foo/");
-    } catch (e) {
-      // errors are fine; this test is only looking for a response
-      response = e.response;
-    }
-    expect(response.status).toBeGreaterThan(1);
-    expect(response.status).toBeLessThan(599);
-    await pathProxy.stop();
-  });
-
-  test("should respond to root path spa request", async () => {
-    await pathProxy.start();
-    let response;
-    try {
-      response = await axios.get("http://localhost:8081/");
-    } catch (e) {
-      // errors are fine; this test is only looking for a response
-      response = e.response;
-    }
-    expect(response.status).toBeGreaterThan(1);
-    expect(response.status).toBeLessThan(599);
-    await pathProxy.stop();
+  // Testing basic HTTP responses for these URL and message combos.  Doesn't test for any particular response, just that
+  // it responds.  Should add a bunch of these.
+  [
+    { msg: "should respond to requests", url: "http://localhost:8084/foo/" },
+    { msg: "should respond to requests with duplicate slash", url: "http://localhost:8084//foo/" },
+    { msg: "should respond to root path spa request", url: "http://localhost:8084/" },
+  ].forEach((target) => {
+    test(target.msg, async () => {
+      await pathProxy.start();
+      let response;
+      try {
+        response = await axios.get(target.url);
+      } catch (e) {
+        // console.log(e);
+        // errors are fine; this test is only looking for a response
+        response = e.response;
+      }
+      expect(response.status).toBeGreaterThan(1);
+      expect(response.status).toBeLessThan(599);
+      await pathProxy.stop();
+    });
   });
 });
