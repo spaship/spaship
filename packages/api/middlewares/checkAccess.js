2dfca8282167ba220d51503c514f55d26d9402d2
Sayak Sarkar <sayak.bugsmith@gmail.com>
2020-10-27 20:17:19 +0530

    fix(api): Pluralize role names.

--- packages/api/middlewares/checkAccess.js
+++ packages/api/middlewares/checkAccess.js
@@ -3,14 +3,14 @@ const config = require("../config");
 module.exports = () => {
   return async (req, res, next) => {
     const roles = {
-      admin: "spaship-admin",
-      user: "spaship-user",
+      admin: "spaship-admins",
+      user: "spaship-users",
     };
     try {
       const propName = config.get("auth:prop");
       if (propName) {
-        roles.admin = "spaship-" + propName + "-admin";
-        roles.user = "spaship-" + propName + "-user";
+        roles.admin = "spaship-" + propName + "-admins";
+        roles.user = "spaship-" + propName + "-users";
       }
       const hasAdminAccess = req.user.role.indexOf(roles.admin) >= 0;
       const hasUserAccess = req.user.role.indexOf(roles.user) >= 0;
