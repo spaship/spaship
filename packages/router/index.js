1daf83b66804bf747be6630fa371a2f9e68c2f0f
Kun Yan <kyan@redhat.com>
2020-11-11 13:17:11 +0800

    remove changeOrigin

--- packages/router/index.js
+++ packages/router/index.js
@@ -110,12 +110,17 @@ const customRouter = function (req) {
 // proxy middleware options
 let options = {
   target: config.get("target"), // target host
-  changeOrigin: true,
   router: customRouter,
   logLevel: config.get("log_level"),
   logProvider: () => log,
   autoRewrite: true,
   onProxyRes: (proxyRes, req) => {
+    const origin = config.get("origin");
+
+    if (origin) {
+      req.headers["host"] = origin;
+    }
+
     if (proxyRes.statusCode >= 301 && proxyRes.statusCode <= 308 && proxyRes.headers["location"]) {
       // When the origin responds with a redirect it's location contains the flat path.
       // This needs to be converted back to the url path. The original conversion is stored
