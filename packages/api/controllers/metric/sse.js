30c3e984c8ba6350be5a45a794ddc1d1b8c1585b
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2021-07-26 12:29:22 +0530

    SSE commit

---
+++ packages/api/controllers/metric/sse.js
@@ -0,0 +1,91 @@
+const event = require('../../models/event')
+const eventTimeTrace = require('../../models/eventTimeTrace')
+const EventSource = require("eventsource");
+const { uuid } = require('uuidv4');
+
+var source = new EventSource("http://localhost:5000/sse/80");
+
+source.onmessage = function (event) {
+    const response = JSON.parse(event.data);
+    const envRandom = getRandom(4);
+    const branchRandom = getRandom(3);
+    Promise.all([createEventRequest(response, envRandom, branchRandom), createEventTimeTraceRequest(response, envRandom)]);
+};
+
+const branch = ["dev", "master", "spaship"];
+const env = ["Dev", "QA", "Stage", "Prod"]
+
+async function createEventRequest(response, envRandomRequest, branchRandomRequest) {
+    const currentTime = new Date();
+    const branchRandom = branchRandomRequest;
+    const envRandom = envRandomRequest;
+    const eventData = new event({
+        id: response.id,
+        eventId: response.id,
+        propertyName: response.payload.namespace,
+        spaName: response.payload.cr_name,
+        version: 1,
+        envs: env[envRandom],
+        branch: branch[branchRandom],
+        code: response.payload.CODE,
+        failure: false,
+        isActive: true,
+        createdAt: response.time || currentTime,
+        updatedAt: response.time || currentTime,
+        traceId: response.payload.TraceId
+    });
+    const saveResponse = await eventData.save();
+    console.log(saveResponse);
+}
+
+async function createEventTimeTraceRequest(response, envRandomRequest) {
+    const currentTime = new Date();
+    const envRandom = envRandomRequest;
+
+    const checkTraceId = await eventTimeTrace.findOne({ traceId: response.payload.TraceId });
+    console.log("Result");
+    console.log(checkTraceId);
+
+    if (checkTraceId == null) {
+        console.log("Executing If");
+        const eventTimeTraceData = new eventTimeTrace({
+            id: uuid(),
+            traceId: response.payload.TraceId,
+            propertyName: response.payload.namespace,
+            spaName: response.payload.cr_name,
+            envs: env[envRandom],
+            initialCode: response.payload.CODE,
+            finalCode: null,
+            failure: true,
+            createdAt: response.time || currentTime,
+            completedAt: null,
+            consumedTime: 0
+        });
+        const saveResponse = await eventTimeTraceData.save();
+        console.log(saveResponse);
+    }
+    else {
+        console.log("Executing Else (Updating Data)");
+
+        let diff = (checkTraceId.createdAt.getTime() - new Date(response.time).getTime()) / 1000;
+        diff /= 60;
+        const consumedTime = Math.abs(Math.round(diff));
+        console.log("Diffrence : " + consumedTime);
+
+        const updated = await eventTimeTrace.findOneAndUpdate({
+            traceId: response.payload.TraceId,
+        }, { $set: { finalCode: response.payload.CODE, completedAt: response.time, consumedTime: consumedTime, failure: false } },
+            function (err, data) {
+                if (err) {
+                    console.log("error");
+                }
+            });
+        console.log(" UPDATED");
+        console.log(updated);
+    }
+}
+
+
+function getRandom(max) {
+    return Math.floor(Math.random() * max);
+}
