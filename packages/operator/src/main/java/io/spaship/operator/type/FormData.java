075af5bc8953f13a6446e74fd6e2b27aaf86da30
Arko <47105136+arkaprovob@users.noreply.github.com>
2021-10-12 00:23:07 -0700

    fix (operator)  (#1428)
    * fix (operator) : on server exception, the API will return a user understandable response body, instead of an error class hash.
    
    * feat(operator) : file validation and send a response on invalid file upload, and creates an event on error
    fix(operator) : throws an exception and stops the flow to avoid Cascading failure.
    
    * fix(operator) : fixed 200 response issue on no file upload.

--- packages/operator/src/main/java/io/spaship/operator/type/FormData.java
+++ packages/operator/src/main/java/io/spaship/operator/type/FormData.java
@@ -8,8 +8,11 @@ import org.jboss.resteasy.reactive.multipart.FileUpload;
 import javax.ws.rs.core.MediaType;
 import java.net.URI;
 import java.nio.file.Path;
+import java.util.Objects;
 
 public class FormData {
+
+  private static final String FILE_NOT_FOUND_MESSAGE = "file not received";
 
     @RestForm
     @PartType(MediaType.TEXT_PLAIN)
@@ -23,23 +26,34 @@ public class FormData {
     public FileUpload file;
 
     public String fileName() {
+        Objects.requireNonNull(file,FILE_NOT_FOUND_MESSAGE);
         return this.file.fileName();
     }
 
     public Long fileSize() {
+        Objects.requireNonNull(file,FILE_NOT_FOUND_MESSAGE);
         return this.file.size();
     }
 
     public String fileLocation() {
+        Objects.requireNonNull(file,FILE_NOT_FOUND_MESSAGE);
         return this.file.uploadedFile().toString();
     }
 
     public URI getFileURI() {
+        Objects.requireNonNull(file,FILE_NOT_FOUND_MESSAGE);
         return this.file.uploadedFile().toUri();
     }
 
     public Path getfilePath() {
+        Objects.requireNonNull(file,FILE_NOT_FOUND_MESSAGE);
         return this.file.uploadedFile().toAbsolutePath();
+    }
+
+    public boolean isFileValid(){
+      Objects.requireNonNull(file,FILE_NOT_FOUND_MESSAGE);
+      Objects.requireNonNull(file.contentType(),"no content found!");
+      return this.file.contentType().contains("zip");
     }
 
 }
