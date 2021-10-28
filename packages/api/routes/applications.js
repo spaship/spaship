aa0c0ef817ea2a6cb6546f5ce1f3d424d27bc8fb
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2021-10-28 21:42:55 +0530

    feat (api) : upload spa api optimized

--- packages/api/routes/applications.js
+++ packages/api/routes/applications.js
@@ -1,8 +1,6 @@
 const { Router } = require("express");
 const application = require("../controllers/application");
 const upload = require("../utils/multerUtil");
-const uploadSpa = require("../controllers/webPropertyServices/uploadSpa");
-const uploadSpaController = require("../controllers/webPropertyServices/uploadSpaService");
 
 const router = new Router();
 
@@ -28,7 +26,6 @@ router.post("/", application.post);
  */
 router.post("/deploy", upload.single("upload"), application.deploy);
 
-router.post("/upload", uploadSpa.single("data"),  uploadSpaController.uploadSpaController);
 /**
  * Update an application by name
  */
