e1eca94d33577c92b47b84434682dce5f83977e4
Sayak Sarkar <sayak.bugsmith@gmail.com>
2022-03-23 01:00:25 +0530

    feat: Add and configure Patternfly.

--- packages/manager/pages/_app.tsx
+++ packages/manager/pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import "@patternfly/react-core/dist/styles/base.css";
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
