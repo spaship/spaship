d3301113534cca09a78aed53225c541b448b53dc
Sayak Sarkar <sayak.bugsmith@gmail.com>
2022-03-23 18:50:33 +0530

    feat(page): Created Login page.

--- packages/manager/README.md
+++ packages/manager/README.md
@@ -20,6 +20,8 @@ You can start editing the page by modifying `pages/index.tsx`. The page auto-upd
 
 The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
 
+## Authentication
+This project uses [NextAuth.js](https://next-auth.js.org/) for Authentication. You can check out [NextAuth.js Tutorials](https://next-auth.js.org/tutorials) to learn about it.
 ## Learn More
 
 To learn more about Next.js, take a look at the following resources:
--- packages/manager/pages/_app.tsx
+++ packages/manager/pages/_app.tsx
@@ -1,13 +1,21 @@
 import { SessionProvider } from "next-auth/react";
+import Head from 'next/head'
 import "@patternfly/react-core/dist/styles/base.css";
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app'
+import Footer from "../layout/footer";
 
 function SPAshipManager({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>SPAship Manager</title>
+        <meta name="description" content="The SPAship User Interface for managing your SPAship properties." />
+        <link rel="icon" href="images/icons/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
+      <Footer></Footer>
     </SessionProvider>
   )
 }
