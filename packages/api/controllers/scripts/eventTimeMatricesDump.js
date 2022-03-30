ba7f7e48a31bd2ceae178ce052c949f5dae0b0f7
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2022-03-30 12:25:52 +0530

    feat (api) : data dump script added

--- packages/api/controllers/scripts/eventTimeMatricesDump.js
+++ packages/api/controllers/scripts/eventTimeMatricesDump.js
@@ -1,15 +1,15 @@
 const eventTimeTrace = require("../../models/eventTimeTrace");
 
 const eventTimeMatricesDump = async (req, res) => {
-  res.status(200).json(await eventTimeMatricesDumpService(req.sanitize(getDocument(req))));
+  res.status(200).json(await eventTimeMatricesDumpService(getDocument(req)));
 };
 
 const eventTimeMatricesDumpService = async (docs) => {
   try {
     const bulk = await eventTimeTrace.collection.initializeUnorderedBulkOp();
     const currentDate = new Date();
-    createBulkRequest(docs, currentDate, bulk);
-    bulk.execute();
+    await createBulkRequest(docs, currentDate, bulk);
+    await bulk.execute();
     return { Success: "All data successfully updated", "Records Number": getDocumentCounts(docs) };
   } catch (e) {
     console.log(e);
@@ -24,13 +24,12 @@ function getDocument(req) {
   return req?.body || [];
 }
 
-function createBulkRequest(docs, currentDate, bulk) {
+async function createBulkRequest(docs, currentDate, bulk) {
   for (i = 0; i < getDocumentCounts(docs); i += 1) {
     docs[i]._id = null;
     if (docs[i].createdAt && docs[i].completedAt) {
       docs[i].createdAt = new Date(docs[i].createdAt.$date);
       docs[i].completedAt = new Date(docs[i].completedAt.$date);
-      console.log(docs[i]);
       bulk.insert(docs[i]);
     }
   }
