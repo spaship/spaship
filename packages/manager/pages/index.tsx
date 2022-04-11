14580a263cc138f49e88eae5e7709a8f4102805d
Sayak Sarkar <sayak.bugsmith@gmail.com>
2022-04-11 23:26:21 +0530

    fix(manager): Fix image asset paths.

--- packages/manager/pages/index.tsx
+++ packages/manager/pages/index.tsx
@@ -2,12 +2,12 @@ import Body from "../components/layout/body";
 import { ComponentWithAuth } from "../utils/auth.utils";
 
 const Home: ComponentWithAuth = () => {
-    return ( 
-        <Body title={"Welcome to SPAship! 👋 "} breadcrumbs={[{ path: "/", title: 'Home' }]}>
-            <div className="page-body">To get started select an option from the left sidebar!</div>
-        </Body>
-     );
-}
+  return (
+    <Body title={"Welcome to SPAship! 👋 "} breadcrumbs={[{ path: "/", title: "Home" }]}>
+      <div className="page-body">To get started select an option from the left sidebar!</div>
+    </Body>
+  );
+};
 
 Home.authenticationEnabled = true;
 export default Home;
--- packages/manager/pages/login/index.tsx
+++ packages/manager/pages/login/index.tsx
@@ -1,35 +1,51 @@
-import type { NextPage } from 'next'
-import Image from 'next/image'
-import Link from 'next/link';
+import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import { useSession, signIn } from "next-auth/react";
-import { FileAltIcon, GithubIcon, OptimizeIcon, UserIcon } from '@patternfly/react-icons';
-import { Bullseye, Button, Card, CardBody, CardHeader, Flex, FlexItem, Page, PageHeader, PageHeaderTools, PageHeaderToolsGroup, PageHeaderToolsItem, PageSection, PageSectionVariants, Title } from '@patternfly/react-core';
-import EmptySpinner from '../../components/general/empty-spinner';
-import styled from 'styled-components';
-import rocket from '../public/images/illustrations/rocket.svg';
-import darkLogo from '../public/images/logo/spaship-logo-dark-vector.svg';
+import { FileAltIcon, GithubIcon, OptimizeIcon, UserIcon } from "@patternfly/react-icons";
+import {
+  Bullseye,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  FlexItem,
+  Page,
+  PageHeader,
+  PageHeaderTools,
+  PageHeaderToolsGroup,
+  PageHeaderToolsItem,
+  PageSection,
+  PageSectionVariants,
+  Title,
+} from "@patternfly/react-core";
+import EmptySpinner from "../../components/general/empty-spinner";
+import styled from "styled-components";
+import rocket from "../../public/images/illustrations/rocket.svg";
+import darkLogo from "../../public/images/logo/spaship-logo-dark-vector.svg";
 
 const Header = styled(PageHeader)({
   backgroundColor: "white",
-  height: "10vh"
+  height: "10vh",
 });
 
 const Defcard = styled.div({
-  padding: "15vh 0 5vh 0",  //TODO: Remove after adding OAuth options.
+  padding: "15vh 0 5vh 0", //TODO: Remove after adding OAuth options.
   width: "500px",
-  margin: "0 5vw 0 15vw"
+  margin: "0 5vw 0 15vw",
 });
 
 const Slogan = styled.h1({
-  fontSize: "2.4rem"
+  fontSize: "2.4rem",
 });
 
 const Description = styled.div({
-  fontSize: "1rem"
+  fontSize: "1rem",
 });
 
 const Body = styled(PageSection)({
-  height: "80vh"
+  height: "80vh",
 });
 
 const StyledCard = styled(Card)({
@@ -39,16 +55,16 @@ const StyledCard = styled(Card)({
   right: "0",
   height: "124px",
   width: "500px",
-  zIndex: 99
+  zIndex: 99,
 });
 
 const StyledButton = styled(Button)({
-  color: "var(--spaship-global--Color--text-black) !important"
+  color: "var(--spaship-global--Color--text-black) !important",
 });
 
 const Footer = styled(PageSection)({
   height: "10vh",
-  backgroundColor: "var(--spaship-global--Color--text-black)"
+  backgroundColor: "var(--spaship-global--Color--text-black)",
 });
 
 const Rocket = styled.div({
@@ -69,28 +85,21 @@ const HeaderTools = () => {
             {session.user.name}
           </StyledButton>
         </PageHeaderToolsItem>
-      )
-    }
-    else 
-    return (
-      <PageHeaderToolsItem>
-        <StyledButton onClick={() => signIn('github', {callbackUrl: '/'})} variant="link" icon={<UserIcon />}>
-          Sign In
-        </StyledButton>
-      </PageHeaderToolsItem>
-    )
-  }
+      );
+    } else
+      return (
+        <PageHeaderToolsItem>
+          <StyledButton onClick={() => signIn("github", { callbackUrl: "/" })} variant="link" icon={<UserIcon />}>
+            Sign In
+          </StyledButton>
+        </PageHeaderToolsItem>
+      );
+  };
   return (
     <PageHeaderTools>
       <PageHeaderToolsGroup>
         <PageHeaderToolsItem>
-          <StyledButton
-            component="a"
-            href="https://spaship.io"
-            target="_blank"
-            variant="link"
-            icon={<FileAltIcon />}
-          >
+          <StyledButton component="a" href="https://spaship.io" target="_blank" variant="link" icon={<FileAltIcon />}>
             Documentation
           </StyledButton>
         </PageHeaderToolsItem>
@@ -108,11 +117,11 @@ const HeaderTools = () => {
         {renderLoginButton()}
       </PageHeaderToolsGroup>
     </PageHeaderTools>
-  )
-}
+  );
+};
 
 const Login: NextPage = () => {
-  const { status: authStatus} = useSession();
+  const { status: authStatus } = useSession();
   if (authStatus === "loading") {
     return (
       <Bullseye>
@@ -121,61 +130,72 @@ const Login: NextPage = () => {
     );
   }
   return (
-    <Page header={<Header logo={<Image src={darkLogo} alt="SPAship Logo" height={"40rem"} width={"200rem"}/>} headerTools={<HeaderTools />} />}>
+    <Page
+      header={
+        <Header
+          logo={<Image src={darkLogo} alt="SPAship Logo" height={"40rem"} width={"200rem"} />}
+          headerTools={<HeaderTools />}
+        />
+      }
+    >
       <Body variant={PageSectionVariants.light} isFilled>
-      <Flex>
         <Flex>
-          <FlexItem>
-            <Defcard>
-              <Slogan>
-                develop fast · <span className='solar-orange'>deploy faster</span>
-              </Slogan>
-              <Description className='sonic-silver'>
-                SPAship is a open source platform for deploying, integrating, and managing single-page apps (SPAs).
-              </Description>
-            </Defcard>
-          </FlexItem>
-        </Flex>
-        <Flex>
-          <FlexItem>
-            <StyledCard>
-              <CardHeader>
-                <Title headingLevel="h6" size="md">
-                  Sign in with
-                </Title>
-              </CardHeader>
-              <CardBody>
-                <Button isBlock variant="primary" onClick={() => signIn('keycloak')} className="spaship_btn">
-                  Red Hat SSO
-                </Button>
-              </CardBody>
-            </StyledCard>
-          </FlexItem>
-        </Flex>
+          <Flex>
+            <FlexItem>
+              <Defcard>
+                <Slogan>
+                  develop fast · <span className="solar-orange">deploy faster</span>
+                </Slogan>
+                <Description className="sonic-silver">
+                  SPAship is a open source platform for deploying, integrating, and managing single-page apps (SPAs).
+                </Description>
+              </Defcard>
+            </FlexItem>
+          </Flex>
+          <Flex>
+            <FlexItem>
+              <StyledCard>
+                <CardHeader>
+                  <Title headingLevel="h6" size="md">
+                    Sign in with
+                  </Title>
+                </CardHeader>
+                <CardBody>
+                  <Button isBlock variant="primary" onClick={() => signIn("keycloak")} className="spaship_btn">
+                    Red Hat SSO
+                  </Button>
+                </CardBody>
+              </StyledCard>
+            </FlexItem>
+          </Flex>
         </Flex>
       </Body>
       <Footer variant={PageSectionVariants.darker} isFilled={false}>
         Brought to you by the{" "}
         <Link href="https://github.com/spaship/spaship/graphs/contributors" passHref>
-          <a className='solar-orange' target="_blank" rel="noreferrer noopener">Wizards <OptimizeIcon /></a>
+          <a className="solar-orange" target="_blank" rel="noreferrer noopener">
+            Wizards <OptimizeIcon />
+          </a>
         </Link>{" "}
         of the{" "}
         <Link href="https://github.com/spaship/spaship">
-          <a className='solar-orange' target="_blank" rel="noreferrer noopener">SPAship</a>
+          <a className="solar-orange" target="_blank" rel="noreferrer noopener">
+            SPAship
+          </a>
         </Link>{" "}
         project. <br /> Code licensed under the{" "}
         <Link href="https://github.com/spaship/spaship/blob/master/LICENSE">
-          <a className='solar-orange' target="_blank" rel="noreferrer noopener">
+          <a className="solar-orange" target="_blank" rel="noreferrer noopener">
             MIT License
           </a>
         </Link>
         .
       </Footer>
       <Rocket>
-        <Image src={rocket} alt="Rocket" priority/>
+        <Image src={rocket} alt="Rocket" priority />
       </Rocket>
     </Page>
-  )
-}
+  );
+};
 
 export default Login;
