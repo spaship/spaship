72b1a563ffabd2f41196d76c5292a93e1b8d6a58
Sayak Sarkar <sayak.bugsmith@gmail.com>
2022-04-11 19:16:33 +0530

    chore(manager): Pre-migration manager repository cleanup and lerna config update.

--- packages/manager/src/components/general/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import { Bullseye, Spinner, SpinnerProps } from "@patternfly/react-core";
-
-export default (props: SpinnerProps) => {
-  return (
-    <Bullseye>
-      <Spinner {...props} />
-    </Bullseye>
-  );
-};
