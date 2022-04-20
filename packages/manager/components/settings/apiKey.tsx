a4a9480724608faa4225fffc85cff75bed3a4019
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2022-04-20 18:57:11 +0530

    feat(apikey) : Api key module in manager (#1645)
    * feat(apikey) : Api key module in manager
    
    * fix(style) : Styled component name changed

--- packages/manager/components/settings/apiKey.tsx
+++ packages/manager/components/settings/apiKey.tsx
@@ -1,17 +1,13 @@
 import {
-  Button, Flex, FlexItem, Form,
-  FormGroup, Modal,
-  ModalVariant, Popover,
-  Text,
-  TextContent,
-  TextInput,
-  TextVariants
+  Button, ClipboardCopy, Flex, FlexItem, Modal,
+  ModalVariant, Text,
+  TextContent, TextVariants
 } from "@patternfly/react-core";
 import React, { FunctionComponent, useEffect, useState } from "react";
-import { post } from "../../utils/api.utils";
-import { getHost } from "../../utils/config.utils";
-import { AnyProps } from "../models/props";
 import styled from "styled-components";
+import { get } from "../../utils/api.utils";
+import { getNextValidateUrl } from "../../utils/endpoint.utils";
+import { AnyProps } from "../models/props";
 
 interface ApiKeyProps { }
 
@@ -32,114 +28,81 @@ const StyledText = styled(Text)`
   --pf-c-content--h2--FontWeight: 100;
 `;
 
+const StyledClipboardBox = styled.div({
+  width: "500px",
+  height: "40px",
+});
+
 const ApiKey: FunctionComponent<ApiKeyProps> = () => {
   const [isModalOpen, setModalOpen] = useState(false);
-  const [env, setEnv] = useState("");
   const [_apiKey, setApiKey] = useState("");
 
   async function handleModalToggle() {
     setModalOpen(!isModalOpen);
   }
 
-  async function onClickMethod() {
-    setModalOpen(!isModalOpen);
-
-    if (isModalOpen == true) {
-      try {
-        const host = getHost();
-        const url = `${host}/applications/validate`;
-        const payload = {};
-        const response = await post<AnyProps>(url, payload);
-        setApiKey(response.token);
-      } catch (e) { }
-    }
-  }
-
-  const handleNameInputChange = (value: string) => {
-    setEnv(value);
-  };
-
   useEffect(() => {
-    // TODO document why this arrow function is empty
+    (async () => {
+      if (isModalOpen == true) {
+        try {
+          const url = getNextValidateUrl();
+          const response = await get<AnyProps>(url);
+          setApiKey(response.token);
+        } catch (e) { }
+      }
+    })()
   }, [isModalOpen]);
 
   const GenerateKeyModal = () => (
     <Modal
       variant={ModalVariant.small}
-      title="Name API Key"
+      title="API Key"
+      description="Please save this API Key"
       isOpen={isModalOpen}
       onClose={handleModalToggle}
-      actions={[
-        <StyledButton
-          key="create"
-          variant="tertiary"
-          onClick={onClickMethod}>
-          Generate API Key
-        </StyledButton>,
-      ]}>
-      <Form id="modal-with-form-form">
-        <FormGroup
-          label="Enter environment name"
-          labelIcon={
-            <Popover bodyContent={null}>
-              <button
-                type="button"
-                aria-label="More info for name field"
-                onClick={(e) => e.preventDefault()}
-                aria-describedby="modal-with-form-form-name"
-                className="pf-c-form__group-label-help"
-              >
-              </button>
-            </Popover>}
-          isRequired
-          fieldId="modal-with-form-form-name">
-          <TextInput
-            isRequired
-            type="email"
-            id="modal-with-form-form-name"
-            name="modal-with-form-form-name"
-            value={env}
-            onChange={handleNameInputChange}
-          />
-        </FormGroup>
-      </Form>
+    >
+      <StyledClipboardBox>
+        <ClipboardCopy hoverTip="Copy" clickTip="Copied" isReadOnly={true} >
+          {_apiKey}
+        </ClipboardCopy>
+      </StyledClipboardBox>
     </Modal>
   );
 
   return (
-  <>
-    <Flex
-      justifyContent={ { default: 'justifyContentSpaceBetween'} }
-      alignItems={ { default: 'alignItemsCenter' } }>
-      <FlexItem>
-        <Flex
-          direction={{ default: 'column' }}>
-          <StyledFlexItem>
-            <TextContent>
-              <StyledText component={TextVariants.h2}>
-                Generate API Key
+    <>
+      <Flex
+        justifyContent={{ default: 'justifyContentSpaceBetween' }}
+        alignItems={{ default: 'alignItemsCenter' }}>
+        <FlexItem>
+          <Flex
+            direction={{ default: 'column' }}>
+            <StyledFlexItem>
+              <TextContent>
+                <StyledText component={TextVariants.h2}>
+                  Generate API Key
+                </StyledText>
+              </TextContent>
+            </StyledFlexItem>
+            <FlexItem>
+              <StyledText component={TextVariants.h4}>
+                This key would be valid for 5 hours only.
               </StyledText>
-            </TextContent>
-          </StyledFlexItem>
-          <FlexItem>
+            </FlexItem>
+          </Flex>
+        </FlexItem>
+        <FlexItem>
+          <StyledButton
+            variant="tertiary"
+            onClick={handleModalToggle}>
             <StyledText component={TextVariants.h4}>
-              This key would be valid for 5 hours only.
-            </StyledText>
-          </FlexItem>
-        </Flex>
-      </FlexItem>
-      <FlexItem>
-        <StyledButton 
-          variant="tertiary"
-          onClick={handleModalToggle}>
-          <StyledText component={TextVariants.h4}>
               Create API key
-          </StyledText>
-        </StyledButton>
-      </FlexItem>
-    </Flex>
-    <GenerateKeyModal />
-  </>
+            </StyledText>
+          </StyledButton>
+        </FlexItem>
+      </Flex>
+      <GenerateKeyModal />
+    </>
   );
 };
 
