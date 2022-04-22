74c2a4ec2f4fd1f474a6fe04b8d85c441456da72
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2022-04-22 13:17:25 +0530

    feat(property): Property onboarding api (#1651)

---
+++ packages/manager/pages/api/webPropertyOnboard.tsx
@@ -0,0 +1,32 @@
+import { AnyProps } from "../../components/models/props";
+import { post } from "../../utils/api.utils";
+import { getOnboardWebpropertyUrl } from "../../utils/endpoint.utils";
+
+const WebPropertyOnboard = async (req: AnyProps, res: AnyProps) => {
+    const url = getOnboardWebpropertyUrl();
+    const propertyType = {
+        operator: "operator",
+    };
+    const payload = {
+        "property": getProperty(req),
+        "env": getEnv(req),
+        "namespace": getNamespace(req),
+        "type": propertyType.operator
+    };
+    const response = await post<AnyProps>(url, payload);
+    return res.send({ data: { response } });
+}
+
+export default WebPropertyOnboard;
+
+function getNamespace(req: any) {
+    return req.body.env;
+}
+
+function getEnv(req: any) {
+    return req.body.env;
+}
+
+function getProperty(req: any) {
+    return req.body.property;
+}
