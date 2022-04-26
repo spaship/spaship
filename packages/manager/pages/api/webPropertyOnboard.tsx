751973a080a5bb96467753da88edea15b2b76f9b
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2022-04-26 16:11:45 +0530

    feat(api) : Webproperty onboard API added

--- packages/manager/pages/api/webPropertyOnboard.tsx
+++ packages/manager/pages/api/webPropertyOnboard.tsx
@@ -1,17 +1,22 @@
 import { AnyProps } from "../../components/models/props";
 import { post } from "../../utils/api.utils";
 import { getOnboardWebpropertyUrl } from "../../utils/endpoint.utils";
+import { getSession } from "next-auth/react";
 
 const WebPropertyOnboard = async (req: AnyProps, res: AnyProps) => {
     const url = getOnboardWebpropertyUrl();
+    const userEmail = (await getSession({ req }) as any).user.email;
     const propertyType = {
         operator: "operator",
     };
     const payload = {
-        "property": getProperty(req),
+        "propertyTitle": getPropertyTitle(req),
+        "propertyName": getPropertyName(req),
         "env": getEnv(req),
+        "url": getUrl(req),
         "namespace": getNamespace(req),
-        "type": propertyType.operator
+        "type": propertyType.operator,
+        "createdBy": userEmail
     };
     const response = await post<AnyProps>(url, payload);
     return res.send({ data: { response } });
@@ -27,6 +32,14 @@ function getEnv(req: any) {
     return req.body.env;
 }
 
-function getProperty(req: any) {
-    return req.body.property;
+function getUrl(req: any) {
+    return req.body.url;
+}
+
+function getPropertyName(req: any) {
+    return req.body.propertyName;
+}
+
+function getPropertyTitle(req: any) {
+    return req.body.propertyTitle;
 }
