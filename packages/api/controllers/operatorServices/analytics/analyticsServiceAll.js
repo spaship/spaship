56802069b1f6c3bbd311befe61f5fce992805c8f
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2022-06-02 09:21:03 +0530

    feat(api) : analytics services moved

---
+++ packages/api/controllers/operatorServices/analytics/analyticsServiceAll.js
@@ -0,0 +1,140 @@
+const getCounts = require("./utils/getCountDeployments");
+const getCountByEnvWeeklyChart = require("./utils/getCountByEnvWeeklyChart");
+const getLatestActivities = require("./utils/getLatestActivities");
+
+const analyticsServiceAll = async (req, res) => {
+  try {
+    const response = await propertyFilteration(req.body);
+    if (response.length === 0) return res.status(200).json({ message: "No data avaliable." });
+    res.status(200).json(response);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const propertyFilteration = async (request) => {
+  console.log(request);
+  if (request?.count) {
+    if (request?.count?.all == true) {
+      return await getCounts.getCountService(
+        { code: "WEBSITE_CREATE" },
+        {
+          propertyName: "$propertyName",
+          code: "$code",
+        },
+        {
+          _id: 0,
+          propertyName: "$_id.propertyName",
+          code: "$_id.code",
+          count: "$count",
+        }
+      );
+    } else if (request?.count?.code == true) {
+      return await getCounts.getCountService(
+        {},
+        {
+          propertyName: "$propertyName",
+          code: "$code",
+        },
+        {
+          _id: 0,
+          propertyName: "$_id.propertyName",
+          code: "$_id.code",
+          count: "$count",
+        }
+      );
+    } else if (request?.count?.spa == true) {
+      return await getCounts.getCountService(
+        {},
+        {
+          propertyName: "$propertyName",
+          spaName: "$spaName",
+        },
+        {
+          _id: 0,
+          propertyName: "$_id.propertyName",
+          spaName: "$_id.spaName",
+          count: "$count",
+        }
+      );
+    }
+  } else if (request?.chart) {
+    if (request?.chart.all == true && request?.chart.property == true && request?.chart.spa == true) {
+      return await getCounts.getCountService(
+        {},
+        {
+          propertyName: "$propertyName",
+          spaName: "$spaName",
+          code: "$code",
+        },
+        {
+          _id: 0,
+          propertyName: "$_id.propertyName",
+          spaName: "$_id.spaName",
+          code: "$_id.code",
+          count: "$count",
+        }
+      );
+    } else if (request?.chart.all == true && request?.chart.property == true) {
+      return await getCounts.getCountService(
+        {
+          code: "WEBSITE_CREATE",
+        },
+        {
+          env: "$env",
+        },
+        {
+          _id: 0,
+          env: "$_id.env",
+          count: "$count",
+        }
+      );
+    } else if (request?.chart.all == true) {
+      return await getCountByEnvWeeklyChart.getCountByEnvWeeklyChartService(
+        {
+          code: "WEBSITE_CREATE",
+        },
+        {
+          env: "$env",
+        },
+        {
+          _id: 0,
+          spaName: "$_id.spaName",
+          env: "$_id.env",
+          count: "$count",
+        }
+      );
+    } else if (request?.activities.all == true) {
+      return await getCountByEnvWeeklyChart.getCountByEnvWeeklyChartService(
+        {
+          code: "WEBSITE_CREATE",
+        },
+        {
+          env: "$env",
+        },
+        {
+          _id: 0,
+          spaName: "$_id.spaName",
+          env: "$_id.env",
+          count: "$count",
+        }
+      );
+    }
+  } else if (request?.activities.all == true) {
+    return await getLatestActivities.getLatestActivitiesService(
+      { code: "WEBSITE_CREATE" },
+      { limit: getLimit(request) },
+      { skip: getSkip(request) }
+    );
+  }
+};
+
+function getLimit(request) {
+  return parseInt(request?.activities.limit) || 15;
+}
+
+function getSkip(request) {
+  return parseInt(request?.activities.skip) || 0;
+}
+
+module.exports = { analyticsServiceAll };
---
+++ packages/api/controllers/operatorServices/analytics/utils/getLatestActivities.js
@@ -0,0 +1,62 @@
+const chart = require("../../../../models/event");
+
+const getLatestActivitiesService = async (matchRequest, limitRequest, skipRequest) => {
+  try {
+    const response = await fetchLatestActivitiesByProperty(matchRequest, limitRequest, skipRequest);
+    bindResponse(response);
+    return response;
+  } catch (e) {
+    return { Error: e };
+  }
+};
+
+function bindResponse(response) {
+  const codeMap = {
+    WEBSITE_CREATE: "deployed over",
+    WEBSITE_DELETE: "deleted from",
+    WEBSITE_UPDATE: "updated from",
+  };
+  let i = 1;
+  response.forEach((item) => {
+    item.id = i++;
+    item.createdAt = item.createdAt.toTimeString().substring(0, 8) + " " + item.createdAt.toDateString();
+    actvitiesText(item, codeMap);
+  });
+}
+
+async function fetchLatestActivitiesByProperty(matchRequest, limitRequest, skipRequest) {
+  return await chart.aggregate([
+    {
+      $match: matchRequest,
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $project: {
+        spaName: "$spaName",
+        propertyName: "$propertyName",
+        code: "$code",
+        branch: "$branch",
+        env: "$env",
+        createdAt: "$createdAt",
+        _id: 0,
+      },
+    },
+    {
+      $limit: limitRequest.limit,
+    },
+    {
+      $skip: skipRequest.skip,
+    },
+  ]);
+}
+
+function actvitiesText(item, codeMap) {
+  item.latestActivityHead = " has been " + item.code + " ";
+  item.latestActivityTail = " at " + item.createdAt.toString().slice(0, 24) + " in " + item.env;
+}
+
+module.exports = { getLatestActivitiesService };
