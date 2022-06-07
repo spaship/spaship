7ff341f7f99aec8168450a97d0084ec8de3ebb28
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2022-06-07 12:21:33 +0530

    feat(api) : Scripts added for deploymentConnection

--- packages/api/routes/scripts/dataDump.js
+++ packages/api/routes/scripts/dataDump.js
@@ -5,6 +5,7 @@ const eventTimeMatricesDump = require("../../controllers/scripts/eventTimeMatric
 const webPropertyDataDump = require("../../controllers/scripts/webPropertyDataDump");
 const aliasDataDump = require("../../controllers/scripts/aliasDataDump");
 const applicationDataDump = require("../../controllers/scripts/applicationDataDump");
+const deploymentConnectionDump = require("../../controllers/scripts/deploymentConnectionDump");
 const router = new Router();
 
 router.post("/events", eventDataDump.eventDataDump);
@@ -13,4 +14,5 @@ router.post("/webproperty", webPropertyDataDump.webPropertyDataDump);
 router.post("/alias", aliasDataDump.aliasDataDump);
 router.post("/remove", clearData.clearData);
 router.post("/applications", applicationDataDump.applicationsService);
+router.post("/deploymentConnection", deploymentConnectionDump.deploymentConnectionDump);
 module.exports = router;
