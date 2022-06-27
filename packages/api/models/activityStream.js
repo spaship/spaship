1ba15e1b805ab0305a67a5a483e9ff912d7fa5c3
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2022-06-27 09:04:34 +0530

    feat(api) : changes in schema

---
+++ packages/api/models/activityStream.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const props = new mongoose.Schema({
+  env: {
+    type: String,
+    required: false,
+  },
+  spaName: {
+    type: String,
+    required: false,
+  },
+});
+
+const activityStream = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    source: {
+      type: String,
+      required: true,
+    },
+    action: {
+      type: String,
+      required: true,
+    },
+    propertyName: {
+      type: String,
+      required: true,
+    },
+    props: props,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("activityStream", activityStream);
--- packages/api/models/event.js
+++ packages/api/models/event.js
@@ -1,66 +1,67 @@
 const mongoose = require("mongoose");
 
-const event = new mongoose.Schema({
-  id: {
-    type: String,
-    required: true,
+const event = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    eventId: {
+      type: String,
+      required: true,
+    },
+    traceId: {
+      type: String,
+      required: true,
+    },
+    propertyName: {
+      type: String,
+      required: false,
+    },
+    spaName: {
+      type: String,
+      required: false,
+    },
+    version: {
+      type: String,
+      required: false,
+    },
+    env: {
+      type: String,
+      required: false,
+    },
+    branch: {
+      type: String,
+      required: false,
+    },
+    code: {
+      type: String,
+      required: false,
+    },
+    accessUrl: {
+      type: String,
+      required: false,
+    },
+    state: {
+      type: String,
+      required: false,
+    },
+    path: {
+      type: String,
+      required: false,
+    },
+    failure: {
+      type: Boolean,
+      required: false,
+    },
+    isActive: {
+      type: Boolean,
+      required: false,
+    },
   },
-  eventId: {
-    type: String,
-    required: true,
-  },
-  traceId: {
-    type: String,
-    required: true,
-  },
-  propertyName: {
-    type: String,
-    required: false,
-  },
-  spaName: {
-    type: String,
-    required: false,
-  },
-  version: {
-    type: String,
-    required: false,
-  },
-  env: {
-    type: String,
-    required: false,
-  },
-  branch: {
-    type: String,
-    required: false,
-  },
-  code: {
-    type: String,
-    required: false,
-  },
-  url: {
-    type: String,
-    required: false,
-  },
-  path: {
-    type: String,
-    required: false,
-  },
-  failure: {
-    type: Boolean,
-    required: false,
-  },
-  isActive: {
-    type: Boolean,
-    required: false,
-  },
-  createdAt: {
-    type: Date,
-    required: false,
-  },
-  udpatedAt: {
-    type: Date,
-    required: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("event", event);
