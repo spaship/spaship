232977fdccb9a4cc62dc1c55a269fedcfd286e67
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2022-08-24 08:57:39 +0530

    feat(api) : scipt for apikeys (#1845)

--- packages/api/routes/scripts/dataDump.js
+++ packages/api/routes/scripts/dataDump.js
@@ -6,6 +6,7 @@ const webPropertyDataDump = require("../../controllers/scripts/webPropertyDataDu
 const aliasDataDump = require("../../controllers/scripts/aliasDataDump");
 const applicationDataDump = require("../../controllers/scripts/applicationDataDump");
 const deploymentConnectionDump = require("../../controllers/scripts/deploymentConnectionDump");
+const apiKeysDump = require("../../controllers/scripts/apiKeysDump");
 const router = new Router();
 
 router.post("/events", eventDataDump.eventDataDump);
@@ -13,6 +14,7 @@ router.post("/eventstime", eventTimeMatricesDump.eventTimeMatricesDump);
 router.post("/webproperty", webPropertyDataDump.webPropertyDataDump);
 router.post("/alias", aliasDataDump.aliasDataDump);
 router.post("/remove", clearData.clearData);
+router.post("/apikeys", apiKeysDump.apiKeysDump);
 router.post("/applications", applicationDataDump.applicationsService);
 router.post("/deploymentConnection", deploymentConnectionDump.deploymentConnectionDump);
 module.exports = router;
