b3ffec2de42a94bb6e6f0579c788f054d1f0accd
akhilmhdh <akhilmhdh@gmail.com>
2022-09-09 16:11:23 +0530

    feat: added nprogress
    chore: removed old manager and added new docker image

--- packages/manager/src/pages/_app.tsx
+++ packages/manager/src/pages/_app.tsx
@@ -1,16 +1,20 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import { useEffect } from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import * as yup from 'yup';
+import NProgress from 'nprogress';
 
 import { AuthGuard } from '@app/context/AuthGuard';
 import type { NextPageWithLayout } from '@app/types';
 
 import '@patternfly/react-core/dist/styles/base.css';
+import 'nprogress/nprogress.css';
 import '@app/styles/globals.css';
-import Head from 'next/head';
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
@@ -43,10 +47,30 @@ yup.addMethod(yup.string, 'alphaNumbericOnly', function noWhitespace() {
   });
 });
 
+NProgress.configure({ showSpinner: false });
+
 const MyApp = ({
   Component,
   pageProps: { session, ...pageProps }
 }: AppPropsWithLayout): JSX.Element => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => NProgress.start();
+
+    const handleStop = () => NProgress.done();
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+
   const { isProtected } = Component;
   const getLayout = Component.getLayout ?? ((page) => page);
 
