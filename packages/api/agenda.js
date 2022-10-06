dcfca9b61833b4382f7989fe63075c2749c8a656
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2022-10-07 00:40:29 +0530

    feat(api) : agenda instance

---
+++ packages/api/agenda.js
@@ -0,0 +1,15 @@
+const Agenda = require("agenda");
+const config = require("./config");
+
+const uri = `mongodb://${config.get("db:mongo:url")}`;
+
+const agenda = new Agenda({
+  db: { address: uri, collection: 'agenda' },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+module.exports = {
+  agenda
+};
