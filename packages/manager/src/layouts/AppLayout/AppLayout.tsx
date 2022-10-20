038ef32dc293f508f8df1610969e71bd8490b6d1
hybridx <hybridx@gmail.com>
2022-10-20 13:01:34 +0530

    manager(banner) - Added banner for eph preview doc

--- packages/manager/src/layouts/AppLayout/AppLayout.tsx
+++ packages/manager/src/layouts/AppLayout/AppLayout.tsx
@@ -1,7 +1,8 @@
 import { ReactNode } from 'react';
 
-import { Flex, FlexItem, Page } from '@patternfly/react-core';
+import { Banner, Flex, FlexItem, Page } from '@patternfly/react-core';
 
+import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 import { SideBar } from './components/SideBar';
 import { Nav } from './components/Nav';
 import { Footer } from './components/Footer';
@@ -15,19 +16,40 @@ interface Props {
  * TODO: The title for layout should be changing based on URLs or can be from a context
  */
 export const AppLayout = ({ children }: Props): JSX.Element => (
-  <Page sidebar={<SideBar />} header={<Nav />}>
-    <Flex
-      direction={{ default: 'column' }}
-      spaceItems={{ default: 'spaceItemsNone' }}
-      style={{ height: '100%' }}
-      flexWrap={{ default: 'nowrap' }}
-    >
-      <FlexItem grow={{ default: 'grow' }}>{children}</FlexItem>
-      <FlexItem>
-        <Footer />
-      </FlexItem>
-    </Flex>
-  </Page>
+  <>
+    <Banner isSticky variant="info">
+      <Flex
+        justifyContent={{ default: 'justifyContentCenter' }}
+        alignItems={{ default: 'alignItemsCenter' }}
+      >
+        <a
+          target="_blank"
+          href="https://source.redhat.com/groups/public/dxp/exd_digital_experience_platforms_dxp_blog/ephemeral_preview_feature_in_spaship"
+          rel="noreferrer"
+        >
+          <ExternalLinkAltIcon
+            style={{
+              marginRight: '0.5rem'
+            }}
+          />
+          Introducing Ephemeral Preview feature in SPAship
+        </a>
+      </Flex>
+    </Banner>
+    <Page sidebar={<SideBar />} header={<Nav />}>
+      <Flex
+        direction={{ default: 'column' }}
+        spaceItems={{ default: 'spaceItemsNone' }}
+        style={{ height: '100%' }}
+        flexWrap={{ default: 'nowrap' }}
+      >
+        <FlexItem grow={{ default: 'grow' }}>{children}</FlexItem>
+        <FlexItem>
+          <Footer />
+        </FlexItem>
+      </Flex>
+    </Page>
+  </>
 );
 
 export const getAppLayout = (page: JSX.Element) => <AppLayout>{page}</AppLayout>;
