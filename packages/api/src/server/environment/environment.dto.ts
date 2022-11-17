61f1e1b3a93b1e7aae184e4ffd344125edb722f3
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2022-11-17 21:29:17 +0530

    fix(api) : changes in sync api

--- packages/api/src/server/environment/environment.dto.ts
+++ packages/api/src/server/environment/environment.dto.ts
@@ -41,6 +41,20 @@ export class SyncEnvironmentDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @Length(MIN.DEFAULT, MAX.PROPERTY, { message: MESSAGE.INVALID_LENGTH, always: true })
+  @Matches(VALIDATION.PROPERTY_IDENTIFIER, { message: MESSAGE.INVALID_PROPERTY_IDENTIFIER, always: true })
+  propertyIdentifier: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @Length(MIN.DEFAULT, MAX.ENV, { message: MESSAGE.INVALID_LENGTH, always: true })
+  @Matches(VALIDATION.ENV, { message: MESSAGE.INVALID_ENV, always: true })
+  env: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   sync: string;
 
   @ApiProperty()
