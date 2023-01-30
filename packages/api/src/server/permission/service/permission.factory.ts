36d1ba9d4e889bf3f3e389c8fe04ade09cce3480
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2023-01-30 15:53:45 +0530

    fix(api) : changes in var names

--- packages/api/src/server/permission/service/permission.factory.ts
+++ packages/api/src/server/permission/service/permission.factory.ts
@@ -36,7 +36,7 @@ export class PermissionFactory {
     return permissions;
   }
 
-  groupPermission(permissions: Permission[], matchRole: number) {
+  groupPermission(permissions: Permission[], allPermissions: number) {
     const groupedPermissions = permissions.reduce((acc, obj) => {
       const key = obj.email;
       const group = acc[key] ?? [];
@@ -45,12 +45,12 @@ export class PermissionFactory {
     const emails = Array.from(new Set(permissions.map((item) => item.email)));
     const groupedResponse = [];
     for (const item of emails) {
-      const tmpPermissions = groupedPermissions[item];
+      const allowedPermissions = groupedPermissions[item];
       const data = {
         email: item,
-        name: tmpPermissions[0].name,
-        role: tmpPermissions.length === matchRole ? ROLE.OWNER : ROLE.USER,
-        details: tmpPermissions
+        name: allowedPermissions[0].name,
+        role: allowedPermissions.length === allPermissions ? ROLE.OWNER : ROLE.USER,
+        details: allowedPermissions
       };
       groupedResponse.push(data);
     }
