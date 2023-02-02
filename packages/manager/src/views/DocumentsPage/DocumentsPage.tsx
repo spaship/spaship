d46d0560b7551634ae7aef8bfa8309f79f352209
kotwani2883 <palakkotwani2883@gmail.com>
2023-02-02 11:19:27 +0530

    Added footer elements

--- packages/manager/src/views/DocumentsPage/DocumentsPage.tsx
+++ packages/manager/src/views/DocumentsPage/DocumentsPage.tsx
@@ -13,50 +13,61 @@ export const DocumentsPage = (): JSX.Element => {
     {
       title: 'SPAship, a quickest way to deploy your SPAs ',
       linkhere:
-        'https://source.redhat.com/groups/public/dxp/exd_digital_experience_platforms_dxp_blog/spaship_a_quickest_way_to_deploy_your_spas'
+        'https://source.redhat.com/groups/public/dxp/exd_digital_experience_platforms_dxp_blog/spaship_a_quickest_way_to_deploy_your_spas',
+      footer: 'Getting-Started'
     },
     {
       title: 'SPAship: Baremetal vs Operator ',
       linkhere:
-        'https://source.redhat.com/groups/public/dxp/exd_digital_experience_platforms_dxp_blog/draft_spaship_baremetal_vs_operator'
+        'https://source.redhat.com/groups/public/dxp/exd_digital_experience_platforms_dxp_blog/draft_spaship_baremetal_vs_operator',
+      footer: 'comparison'
     },
     {
       title: 'What is New In SPAship ',
-      linkhere: 'https://source.redhat.com/groups/public/spaship/blog_article/whats_new_in_spaship_'
+      linkhere:
+        'https://source.redhat.com/groups/public/spaship/blog_article/whats_new_in_spaship_',
+      footer: 'Getting-Started'
     },
     {
       title: 'Introducing Ephemeral Preview feature in SPAship ',
       linkhere:
-        'https://source.redhat.com/groups/public/dxp/exd_digital_experience_platforms_dxp_blog/ephemeral_preview_feature_in_spaship'
+        'https://source.redhat.com/groups/public/dxp/exd_digital_experience_platforms_dxp_blog/ephemeral_preview_feature_in_spaship',
+      footer: 'Getting-Started'
     },
     {
       title: 'Getting Deployment Status in Reltime',
       linkhere:
-        'https://source.redhat.com/groups/public/dxp/exd_digital_experience_platforms_dxp_blog/get_your_deployment_status_in_realtime'
+        'https://source.redhat.com/groups/public/dxp/exd_digital_experience_platforms_dxp_blog/get_your_deployment_status_in_realtime',
+      footer: 'Getting-Started'
     },
     {
       title: 'Introducing Sync service in SPAship ',
       linkhere:
-        'https://source.redhat.com/groups/public/dxp/exd_digital_experience_platforms_dxp_blog/introducing_sync_service_in_spaship'
+        'https://source.redhat.com/groups/public/dxp/exd_digital_experience_platforms_dxp_blog/introducing_sync_service_in_spaship',
+      footer: 'Getting-Started'
     },
     {
       title: 'SPAship Manager & CLI Guide ',
-      linkhere: 'https://drive.google.com/file/d/150OyktZdmqMXKwNS1mDkqIo7ZvgyCCGp/view'
+      linkhere: 'https://drive.google.com/file/d/150OyktZdmqMXKwNS1mDkqIo7ZvgyCCGp/view',
+      footer: 'Getting-Started'
     },
     {
       title: 'SPAship CLI Instructions ',
       linkhere:
-        'https://drive.google.com/file/d/1G4B8AKu_8M6fVEMd8WtXHgLhf__Xl2mt/view?usp=share_link'
+        'https://drive.google.com/file/d/1G4B8AKu_8M6fVEMd8WtXHgLhf__Xl2mt/view?usp=share_link',
+      footer: 'Getting-Started'
     },
     {
       title: 'SPAship Release Logs ',
       linkhere:
-        'https://docs.google.com/document/u/1/d/1tdwBCT9d3n3lJnjpiD7B67304Tgl2URwnKwSwGasNHk/edit'
+        'https://docs.google.com/document/u/1/d/1tdwBCT9d3n3lJnjpiD7B67304Tgl2URwnKwSwGasNHk/edit',
+      footer: 'Getting-Started'
     },
     {
       title: 'SPAship FAQ',
       linkhere:
-        'https://docs.google.com/document/d/143ezNXxfujOiTe3VD0cc0ZBYL92F_fSpYCFJUzzDoiQ/edit'
+        'https://docs.google.com/document/d/143ezNXxfujOiTe3VD0cc0ZBYL92F_fSpYCFJUzzDoiQ/edit',
+      footer: 'Getting-Started'
     }
   ];
   return (
