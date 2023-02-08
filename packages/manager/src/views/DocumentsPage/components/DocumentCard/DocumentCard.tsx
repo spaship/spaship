9dd9c10cd307638ec2a8d5390c18db8395779ab8
kotwani2883 <palakkotwani2883@gmail.com>
2023-02-08 10:32:50 +0530

    Consolidating all documents at single place

--- packages/manager/src/views/DocumentsPage/components/DocumentCard/DocumentCard.tsx
+++ packages/manager/src/views/DocumentsPage/components/DocumentCard/DocumentCard.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { Card, CardBody, CardFooter, CardTitle, Text, Title } from '@patternfly/react-core';
 import { css } from '@patternfly/react-styles';
+import { PlayIcon } from '@patternfly/react-icons';
 
 type Props = {
   title?: ReactNode;
@@ -8,24 +9,35 @@ type Props = {
   children?: ReactNode;
   footer?: ReactNode;
   linkhere?: string;
+  isIcon?: boolean;
+  target?: string;
 };
 
-export const DocumentCard = ({ title, subtitle, children, footer, linkhere }: Props) => (
+export const DocumentCard = ({
+  title,
+  subtitle,
+  children,
+  footer,
+  linkhere,
+  isIcon,
+  target
+}: Props) => (
   <a
     href={linkhere}
-    target="_blank"
+    target={target}
     rel="noreferrer"
     style={{ textDecoration: 'none', color: 'black' }}
   >
     <Card
       isSelectable
       isFullHeight
-      style={{ height: '200px', overflow: 'hidden' }}
+      style={{ height: '205px', overflow: 'hidden' }}
       isRounded
       className={css('pf-u-px-sm rounded-md transition hover:shadow-sm')}
     >
       <CardTitle>
         <Title headingLevel="h3" size="xl" className="capitalize">
+          {isIcon ? <PlayIcon style={{ marginRight: '10px', fontSize: '15px' }} /> : null}
           {title}
         </Title>
         <Text component="small" className="pf-u-color-400">
@@ -45,5 +57,7 @@ DocumentCard.defaultProps = {
   title: '',
   subtitle: '',
   footer: '',
-  linkhere: ''
+  linkhere: '',
+  isIcon: false,
+  target: '_blank'
 };
