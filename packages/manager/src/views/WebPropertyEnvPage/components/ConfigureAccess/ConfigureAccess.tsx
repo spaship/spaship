63bfe521da6cf80e61545592135b983fbcf9237e
Nikhita More <nikhitamore1@gmail.com>
2023-02-09 19:04:01 +0530

    fixing pascal case, delete button disable for user

--- packages/manager/src/views/WebPropertyEnvPage/components/ConfigureAccess/ConfigureAccess.tsx
+++ packages/manager/src/views/WebPropertyEnvPage/components/ConfigureAccess/ConfigureAccess.tsx
@@ -11,10 +11,10 @@ import {
   SplitItem
 } from '@patternfly/react-core';
 import { TableComposable, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
-import React from 'react';
+import React, { useEffect } from 'react';
 import toast from 'react-hot-toast';
 import * as yup from 'yup';
-
+import { toPascalCase } from '@app/utils/toPascalConvert';
 export const schema = yup.object({
   // TODO: change this to URL validation, after server supports http protocol append
   url: yup.string().label('Hostname URL').trim().required().max(250),
@@ -28,7 +28,7 @@ export const schema = yup.object({
     .required(),
   cluster: yup.string().label('Environment Type').oneOf(['preprod', 'prod']).required()
 });
-export interface FormData extends yup.InferType<typeof schema> {}
+export interface FormData extends yup.InferType<typeof schema> { }
 
 type GroupItem = {
   email: string;
@@ -68,21 +68,36 @@ type Props = {
   onClose: () => void;
   editMemberName: string;
   propertyIdentifier: string;
-  memberList: Object;
+  memberList: any;
+  flagOpen: boolean;
 };
 
 export const ConfigureAccess = ({
   onClose,
   editMemberName,
   propertyIdentifier,
-  memberList
+  memberList,
+  flagOpen
 }: Props): JSX.Element => {
   const [expanded, setExpanded] = React.useState('def-list-toggle2');
   const [addAccess, setaddAccess] = React.useState([]);
   const [deleteAccess, setdeleteAccess] = React.useState([]);
   const [isShowAdvancedViewEnabled, setIsShowAdvancedViewEnabled] = React.useState(false);
+  let memberCopy = new Object(memberList)
+
+
   const [group, setGroup] = React.useState<any>(memberList);
   const deleteMember = useDeleteMember(propertyIdentifier);
+
+
+  useEffect(() => {
+    // 
+    setGroup(memberCopy)
+
+
+  }, [flagOpen])
+
+
 
   const columnNames2 = {
     NAME: 'Name',
@@ -112,6 +127,7 @@ export const ConfigureAccess = ({
 
     temp.filter((e: GroupItem) => e.email === email)[0][target.name] = checked;
     setGroup({ data: temp });
+
   };
 
   const useAddPermission1 = useAddPermission(propertyIdentifier);
@@ -131,17 +147,21 @@ export const ConfigureAccess = ({
     });
     addData.propertyIdentifier = propertyIdentifier;
     addData.permissionDetails = addPerm;
-    try {
-      useAddPermission1.mutateAsync({
-        ...addData
-      });
-      toast.success('Permission added successfully');
-    } catch (error) {
-      toast.error('Permission not added ');
-    }
+    // if (addPerm.length !== 0) {
+    //   try {
+    //     useAddPermission1.mutateAsync({
+    //       ...addData
+    //     });
+    //     toast.success('Permission updated successfully');
+    //   } catch (error) {
+    //     toast.error('Permission not added ');
+    //   }
+    // }
+
     /// /=========delte
     const deleteData: any = {};
     const deletePerm: any = [];
+
     group.data.map((v: any, k: number) => {
       const tempDelete: any = {};
       const tempActionsDelete: string[] = [];
@@ -152,18 +172,30 @@ export const ConfigureAccess = ({
       tempDelete.actions = tempActionsDelete;
       deletePerm.push(tempDelete);
     });
+
+
     deleteData.propertyIdentifier = propertyIdentifier;
     deleteData.permissionDetails = deletePerm;
-    try {
-      // useDeletePermission(deleteData);
+    console.log("data c", deleteData, addData)
 
-      deleteMember.mutateAsync({
-        ...deleteData
-      });
-      toast.success('Permission deleted successfully');
+    try {
+      if (addPerm.length !== 0) {
+        useAddPermission1.mutateAsync({
+          ...addData
+        });
+      }
+      if (deletePerm.length !== 0) {
+        deleteMember.mutateAsync({
+          ...deleteData
+        });
+      }
+      onClose()
+      toast.success('Permission updated successfully');
     } catch (error) {
       toast.error('Permission not deletd ');
     }
+
+
   };
   return (
     <div>
@@ -179,16 +211,16 @@ export const ConfigureAccess = ({
       </Button>
       {isShowAdvancedViewEnabled && Object.keys(columnNames2).length ? (
         <TableComposable>
-          <Thead>
+          <Thead noWrap={true}>
             <Tr>
               <Th>{columnNames2.NAME}</Th>
               {/* <Th>{columnNames2.ROLE}</Th> */}
-              <Th>{columnNames2.APIKEY_CREATION}</Th>
-              <Th>{columnNames2.APIKEY_DELETION}</Th>
-              <Th>{columnNames2.PERMISSION_CREATION}</Th>
-              <Th>{columnNames2.PERMISSION_DELETION}</Th>
-              <Th>{columnNames2.ENV_CREATION}</Th>
-              <Th>{columnNames2.ENV_SYNC}</Th>
+              <Th>{toPascalCase(columnNames2.APIKEY_CREATION)}</Th>
+              <Th>{toPascalCase(columnNames2.APIKEY_DELETION)}</Th>
+              <Th>{toPascalCase(columnNames2.PERMISSION_CREATION)}</Th>
+              <Th>{toPascalCase(columnNames2.PERMISSION_DELETION)}</Th>
+              <Th>{toPascalCase(columnNames2.ENV_CREATION)}</Th>
+              <Th>{toPascalCase(columnNames2.ENV_SYNC)}</Th>
             </Tr>
           </Thead>
           <Tbody>
@@ -202,7 +234,7 @@ export const ConfigureAccess = ({
                       onChange={(checked, e) => handleChange(checked, e, i.name, i.email)}
                       id="APIKEY_CREATION"
                       name="APIKEY_CREATION"
-                      // onClick={(e) => handleChange1(i.name, i.email, e)}
+                    // onClick={(e) => handleChange1(i.name, i.email, e)}
                     />
                   </Td>
                   <Td>
@@ -211,7 +243,7 @@ export const ConfigureAccess = ({
                       onChange={(checked, e) => handleChange(checked, e, i.name, i.email)}
                       id="APIKEY_DELETION"
                       name="APIKEY_DELETION"
-                      // onClick={(e) => handleChange1(i.name, i.email, e)}
+                    // onClick={(e) => handleChange1(i.name, i.email, e)}
                     />
                   </Td>
                   <Td>
@@ -220,7 +252,7 @@ export const ConfigureAccess = ({
                       onChange={(checked, e) => handleChange(checked, e, i.name, i.email)}
                       id="PERMISSION_CREATION"
                       name="PERMISSION_CREATION"
-                      // onClick={handleChange1(i)}
+                    // onClick={handleChange1(i)}
                     />
                   </Td>
                   <Td>
@@ -229,7 +261,7 @@ export const ConfigureAccess = ({
                       onChange={(checked, e) => handleChange(checked, e, i.name, i.email)}
                       id="PERMISSION_DELETION"
                       name="PERMISSION_DELETION"
-                      // onClick={handleChange1(i)}
+                    // onClick={handleChange1(i)}
                     />
                   </Td>
                   <Td>
@@ -238,7 +270,7 @@ export const ConfigureAccess = ({
                       onChange={(checked, e) => handleChange(checked, e, i.name, i.email)}
                       id="ENV_CREATION"
                       name="ENV_CREATION"
-                      // onClick={handleChange1(i)}
+                    // onClick={handleChange1(i)}
                     />
                   </Td>
                   <Td>
@@ -247,7 +279,7 @@ export const ConfigureAccess = ({
                       onChange={(checked, e) => handleChange(checked, e, i.name, i.email)}
                       id="ENV_SYNC"
                       name="ENV_SYNC"
-                      // onClick={handleChange1(i)}
+                    // onClick={handleChange1(i)}
                     />
                   </Td>
                 </Tr>
@@ -282,16 +314,16 @@ export const ConfigureAccess = ({
                     isHidden={expanded !== `def-list-toggle1_${i.name}`}
                   >
                     <TableComposable>
-                      <Thead>
+                      <Thead noWrap={true}>
                         <Tr>
                           <Th>{columnNames2.NAME}</Th>
                           {/* <Th>{columnNames2.ROLE}</Th> */}
-                          <Th>{columnNames2.APIKEY_CREATION}</Th>
-                          <Th>{columnNames2.APIKEY_DELETION}</Th>
-                          <Th>{columnNames2.PERMISSION_CREATION}</Th>
-                          <Th>{columnNames2.PERMISSION_DELETION}</Th>
-                          <Th>{columnNames2.ENV_CREATION}</Th>
-                          <Th>{columnNames2.ENV_SYNC}</Th>
+                          <Th>{toPascalCase(columnNames2.APIKEY_CREATION)}</Th>
+                          <Th>{toPascalCase(columnNames2.APIKEY_DELETION)}</Th>
+                          <Th>{toPascalCase(columnNames2.PERMISSION_CREATION)}</Th>
+                          <Th>{toPascalCase(columnNames2.PERMISSION_DELETION)}</Th>
+                          <Th>{toPascalCase(columnNames2.ENV_CREATION)}</Th>
+                          <Th>{toPascalCase(columnNames2.ENV_SYNC)}</Th>
                         </Tr>
                       </Thead>
                       <Tbody>
