838f10dab734229d945624a0ef88213e05ce16c6
Nikhita More <nikhitamore1@gmail.com>
2023-03-09 11:25:14 +0530

    adding regex for having numbers and dashes in env name

--- packages/manager/src/views/WebPropertyEnvPage/components/CreateAPIKeyForm/CreateAPIKeyForm.tsx
+++ packages/manager/src/views/WebPropertyEnvPage/components/CreateAPIKeyForm/CreateAPIKeyForm.tsx
@@ -19,7 +19,15 @@ import {
 export const schema = yup.object({
   label: yup.string().label('Label').trim().max(50).required(),
   env: yup
-    .array(yup.string().label('Environment').noWhitespace().trim().alphabetsOnly().required())
+  .array(
+    yup
+      .string()
+      .label('Environment')
+      .trim()
+      .noWhitespace()
+      .matches(/^[a-zA-Z0-9-]+$/, 'Only letters, numbers, and dashes are allowed')
+      .required()
+  )
     .label('Environments')
     .min(1)
     .required(),
