2e045dbc93ac5a9f0ae563080085322ab9b45792
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2023-03-16 16:44:14 +0530

    feat(api) : documentation services added

---
+++ packages/api/src/server/document/documentation.entity.ts
@@ -0,0 +1,17 @@
+export class Documentation {
+  title: string;
+
+  link: string;
+
+  tags: string;
+
+  section: string;
+
+  isVideo: boolean;
+
+  createdBy: string;
+
+  updatedBy: string;
+
+  _id: string;
+}
