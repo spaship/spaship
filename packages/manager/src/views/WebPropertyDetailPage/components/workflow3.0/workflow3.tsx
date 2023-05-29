f43f66f31ef2a1a9584426273e006a7df7cfee3c
Nikhita More <nikhitamore1@gmail.com>
2023-05-29 11:20:40 +0530

    fixing the key value delete from last issue

--- packages/manager/src/views/WebPropertyDetailPage/components/workflow3.0/workflow3.tsx
+++ packages/manager/src/views/WebPropertyDetailPage/components/workflow3.0/workflow3.tsx
@@ -263,6 +263,7 @@ export const Workflow3 = ({
 
   const handleAddConfig = () => {
     appendConfig({ key: '', value: '' });
+    console.log('append', configFields);
   };
 
   const handleAddBuildArgs = () => {
@@ -861,6 +862,7 @@ export const Workflow3 = ({
                     </Button>
                   </SplitItem>
                 </Split>
+
                 {configFields.map((pair, index) => (
                   <Split key={pair.id} hasGutter>
                     <SplitItem key={pair.id} isFilled className="pf-u-mr-md pf-u-mb-lg">
