30e5c2c1401355d5093bcdf018d8b485213b7fff
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2023-06-15 16:41:16 +0530

    fix(api) : change function name

--- packages/api/src/server/application/service/application.factory.ts
+++ packages/api/src/server/application/service/application.factory.ts
@@ -157,8 +157,8 @@ export class ApplicationFactory {
     applicationResponse.env = application.env;
     applicationResponse.ref = this.getRef(application.nextRef);
     applicationResponse.accessUrl = application.isContainerized
-      ? this.getContainerizedAccessUrl(application, baseUrl)
-      : this.getAccessUrl(application, baseUrl);
+      ? this.generateContainerizedAccessUrl(application, baseUrl)
+      : this.generateAccessUrl(application, baseUrl);
     if (applicationExists)
       applicationResponse.warning = `SPA(s) - ${applicationExists} already exist(s) on the context path ${applicationResponse.path}. Overriding existing deployment.`;
     return applicationResponse;
@@ -169,7 +169,7 @@ export class ApplicationFactory {
     return nextRef;
   }
 
-  private getAccessUrl(application: Application, baseUrl: string): string {
+  private generateAccessUrl(application: Application, baseUrl: string): string {
     const protocol = 'http';
     const { hostname } = new URL(baseUrl);
     const appPrefix = hostname.split('.')[4];
@@ -561,7 +561,7 @@ export class ApplicationFactory {
     return gitUrl;
   }
 
-  private getContainerizedAccessUrl(application: Application, baseUrl: string): string {
+  private generateContainerizedAccessUrl(application: Application, baseUrl: string): string {
     const protocol = 'https';
     const { hostname } = new URL(baseUrl);
     const domain = hostname.split('.').slice(1).join('.');
