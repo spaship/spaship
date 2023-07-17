2d95e5529fe14a30f95e3fa06334e1a0f4a01d9d
Nikhita More <nikhitamore1@gmail.com>
2023-07-17 19:33:28 +0530

    logs for wf2.0

--- packages/manager/src/views/WebPropertyDetailPage/components/SSR/ViewLogs.tsx
+++ packages/manager/src/views/WebPropertyDetailPage/components/SSR/ViewLogs.tsx
@@ -39,17 +39,11 @@ export const ViewLogs = ({ propertyIdentifier, spaName, env, type, idList, isGit
   const [logsData, setLogsData] = useState<string>('');
   const [isLogsLoading, setIsLogsLoading] = useState<boolean>(true);
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const { data: podList, isError: isErrorPods } = useListOfPods(propertyIdentifier, spaName, env);
+  const podList = useListOfPods(propertyIdentifier, spaName, env);
 
-  const [selectedId, setSelectedId] = useState<string | undefined>(
-    idList && idList.length > 0 ? idList[idList.length - 1] : undefined
-  );
+  const [selectedId, setSelectedId] = useState<string | undefined>(idList && idList.reverse()[0]);
 
-  const {
-    data: logs,
-    isLoading: isFetchingLogs,
-    isError: isLogsforSpaError
-  } = useGetLogsforSpa(
+  const { data: logs, isLoading: isFetchingLogs } = useGetLogsforSpa(
     propertyIdentifier,
     spaName,
     env,
@@ -58,29 +52,19 @@ export const ViewLogs = ({ propertyIdentifier, spaName, env, type, idList, isGit
   );
 
   useEffect(() => {
-    setSelectedId(idList && idList.length > 0 ? idList[idList.length - 1] : undefined);
-    setLogsData('');
-    setIsLogsLoading(true);
-  }, [idList, type, isGit, isFetchingLogs, isErrorPods]);
-
-  useEffect(() => {
-    if (!isFetchingLogs && logs) {
-      setLogsData(logs);
-      setIsLogsLoading(false);
-    }
+    setLogsData(logs);
+    setIsLogsLoading(isFetchingLogs);
   }, [logs, isFetchingLogs]);
 
   useEffect(() => {
-    if (isLogsforSpaError) {
-      setLogsData('');
-      setIsLogsLoading(false);
-    }
-  }, [isLogsforSpaError]);
+    setSelectedId(idList && idList.reverse()[0]);
+    setLogsData('');
+    setIsLogsLoading(true);
+  }, [idList, type]);
 
   const handleToggle = (isSelectOpen: boolean) => {
     setIsOpen(isSelectOpen);
   };
-
   const handleSelect = (
     event: React.MouseEvent | React.ChangeEvent,
     selection: string | SelectOptionObject,
@@ -91,24 +75,42 @@ export const ViewLogs = ({ propertyIdentifier, spaName, env, type, idList, isGit
     }
     setSelectedId(selection as string);
     setIsOpen(false);
-    setIsLogsLoading(true);
   };
+
   return (
     <div>
       <div className="pf-u-mb-md pf-u-mt-md">
         {toPascalCase(type === 0 ? logType.POD : logType.BUILD)} Logs for <b>{spaName}</b>
       </div>
-
       {!isGit && type === 1 ? (
         <EmptyState>
           <EmptyStateIcon icon={CubesIcon} />
           <Title headingLevel="h4" size="lg">
-            No {toPascalCase(logType.POD)} logs found for <b>{spaName}</b> spa.
+            No {toPascalCase(logType.BUILD)} logs found for <b>{spaName}</b> spa.
           </Title>
         </EmptyState>
-      ) : (
+      ) : !isLogsLoading ? (
         <div>
-          {isErrorPods ? (
+          {idList && (
+            <Select
+              className="custom-select"
+              variant="single"
+              aria-label="Select Input"
+              onToggle={handleToggle}
+              onSelect={handleSelect}
+              selections={selectedId}
+              isOpen={isOpen}
+            >
+              {type === 1
+                ? idList && idList.map((item: string) => <SelectOption key={item} value={item} />)
+                : (podList?.data || []).map((item: string) => (
+                    <SelectOption key={item} value={item} />
+                  ))}
+            </Select>
+          )}
+          {logsData ? (
+            <CodeBlock className="pf-u-mt-md">{NewlineText(logsData)}</CodeBlock>
+          ) : (
             <EmptyState>
               <EmptyStateIcon icon={CubesIcon} />
               <Title headingLevel="h4" size="lg">
@@ -116,44 +118,12 @@ export const ViewLogs = ({ propertyIdentifier, spaName, env, type, idList, isGit
                 <b>{spaName}</b> spa.
               </Title>
             </EmptyState>
-          ) : isLogsLoading ? (
-            <EmptyState>
-              <Spinner className="pf-u-mt-lg" />
-            </EmptyState>
-          ) : (
-            <>
-              {idList && (
-                <Select
-                  className="custom-select"
-                  variant="single"
-                  aria-label="Select Input"
-                  onToggle={handleToggle}
-                  onSelect={handleSelect}
-                  selections={selectedId}
-                  isOpen={isOpen}
-                >
-                  {type === 1
-                    ? idList &&
-                      idList.map((item: string) => <SelectOption key={item} value={item} />)
-                    : (podList?.data || []).map((item: string) => (
-                        <SelectOption key={item} value={item} />
-                      ))}
-                </Select>
-              )}
-              {logsData ? (
-                <CodeBlock className="pf-u-mt-md">{NewlineText(logsData)}</CodeBlock>
-              ) : (
-                <EmptyState>
-                  <EmptyStateIcon icon={CubesIcon} />
-                  <Title headingLevel="h4" size="lg">
-                    No {toPascalCase(type === 0 ? logType.POD : logType.BUILD)} logs found for{' '}
-                    <b>{spaName}</b> spa.
-                  </Title>
-                </EmptyState>
-              )}
-            </>
           )}
         </div>
+      ) : (
+        <EmptyState>
+          <Spinner className="pf-u-mt-lg" />
+        </EmptyState>
       )}
     </div>
   );
