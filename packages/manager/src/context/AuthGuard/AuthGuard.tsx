d4163939c06e57a5481ce8ebf5fb8fc00ca9124c
Nikhita More <nikhitamore1@gmail.com>
2023-11-23 11:19:06 +0530

    feat(Manager): Logout when token expires (#2512)
    * adding support for multiple symlinks and fixing textbox
    
    * fixing lint
    
    * using unauthenticated
    
    * Adding global callback for unauthorized request to logout user
    
    * adding ? session

--- packages/manager/src/context/AuthGuard/AuthGuard.tsx
+++ packages/manager/src/context/AuthGuard/AuthGuard.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-
 import { setOrchestratorAuthorizationHeader } from '@app/config/orchestratorReq';
 import { Bullseye, Page, PageSection, Spinner } from '@patternfly/react-core';
 import { pageLinks } from '@app/links';
@@ -15,18 +14,19 @@ export const AuthGuard = ({ children }: Props) => {
   const { data: session, status } = useSession({
     required: true,
     onUnauthenticated: () => {
+      // Redirect to the login page if the user is not authenticated or if the JWT has expired.
       router.push(pageLinks.loginPage);
     }
   });
 
-  // set the access token header when available for orchestrator requests
+  // Set the access token header when available for orchestrator requests
   useEffect(() => {
     if (session?.accessToken) {
       setOrchestratorAuthorizationHeader(session?.accessToken);
     }
   }, [session?.accessToken]);
 
-  // if the refresh token failed in backend. Signout and ask user to login
+  // If the refresh token failed in the backend, sign out and ask the user to login
   useEffect(() => {
     if (session?.error) {
       signOut({ redirect: false, callbackUrl: pageLinks.loginPage }).then((data) => {
@@ -36,6 +36,7 @@ export const AuthGuard = ({ children }: Props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [session?.error]);
 
+  // Loading state or error state
   if (status === 'loading' || session?.error) {
     return (
       <Page>
@@ -48,5 +49,6 @@ export const AuthGuard = ({ children }: Props) => {
     );
   }
 
+  // If the JWT is valid and the user is authenticated, render the children.
   return children;
 };
