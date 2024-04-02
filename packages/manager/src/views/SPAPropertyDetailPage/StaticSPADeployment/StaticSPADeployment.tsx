99ec9626d88d52398154fc097a11e89bba05b638
Nikhita More <nikhitamore1@gmail.com>
2024-04-02 11:58:12 +0530

    feat(Manager): Fixed the review for PR

--- packages/manager/src/views/SPAPropertyDetailPage/StaticSPADeployment/StaticSPADeployment.tsx
+++ packages/manager/src/views/SPAPropertyDetailPage/StaticSPADeployment/StaticSPADeployment.tsx
@@ -208,7 +208,6 @@ export const StaticSPADeployment = (): JSX.Element => {
 
   const onSelectDataListItem = (id: string) => {
     const index = parseInt(id.charAt(id.length - 1), 10);
-    // const index = parseInt(id.replace('data-list-item', ''), 10) - 1; // Get the index from the id
     const rowSelectedData = paginatedData && paginatedData[index];
     setSelectedData(rowSelectedData);
     setSelectedDataListItemId(id);
