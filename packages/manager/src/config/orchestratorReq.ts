7325f051d4d4b51a41e101fbf05d0983d7056424
Nikhita More <nikhitamore1@gmail.com>
2024-05-20 14:27:17 +0530

    feat(Manager): fFixing logout issue in spaship

--- packages/manager/src/config/orchestratorReq.ts
+++ packages/manager/src/config/orchestratorReq.ts
@@ -1,5 +1,41 @@
 import axios from 'axios';
 import { env } from './env';
+import url from 'url';
+import { getSession } from 'next-auth/react';
+
+async function refreshAccessToken(token: any) {
+  try {
+    if (Date.now() > token.refreshTokenExpired) {
+      return { ...token, error: 'RefreshAccessTokenError' };
+    }
+
+    const params = new url.URLSearchParams({
+      client_id: env.SPASHIP_AUTH_KEYCLOAK_ID,
+      client_secret: env.SPASHIP_AUTH_KEYCLOAK_SECRET,
+      grant_type: 'refresh_token',
+      refresh_token: token.refreshToken
+    });
+
+    const { data } = await axios.post(
+      env.SPASHIP_AUTH_KEYCLOAK_REFRESH_TOKEN_URL,
+      params.toString()
+    );
+
+    return {
+      ...token,
+      accessToken: data.access_token,
+      accessTokenExpired: Date.now() + (data.expires_in - 15) * 1000,
+      refreshToken: data.refresh_token ?? token.refreshToken,
+      refreshTokenExpired: Date.now() + (data.refresh_expires_in - 15) * 1000
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError'
+    };
+  }
+}
 
 export const newOrchestratorRequest = () => {
   const req = axios.create({
@@ -29,6 +65,22 @@ export const deleteOrchestratorAuthorizationHeader = orchReq.removeToken;
 
 // Global error handler for axios requests
 orchestratorReq.interceptors.response.use(
-  (response) => response,
-  (error) => Promise.reject(error)
+  async (response) => response,
+  async (error) => {
+    console.log('error 401', error);
+    if (error.response && error.response.status === 401) {
+      const session = await getSession();
+      if (session && session.accessToken) {
+        const newToken = await refreshAccessToken(session.accessToken);
+        // Update the session with the new token
+        session.accessToken = newToken.accessToken;
+
+        // Retry the original request with the new token
+        setOrchestratorAuthorizationHeader(newToken.accessToken);
+        error.config.headers['Authorization'] = `Bearer ${newToken.accessToken}`;
+        return axios(error.config);
+      }
+    }
+    return Promise.reject(error);
+  }
 );
