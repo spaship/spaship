b71522135ab3f5de00010473a46c2cf1329a7afb
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2024-10-17 18:02:09 +0530

    feat(api): auth guard changes for RBAC 2.0

--- packages/api/src/server/auth/guard.ts
+++ packages/api/src/server/auth/guard.ts
@@ -3,9 +3,10 @@ import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 import * as crypto from 'crypto';
 import jwt_decode from 'jwt-decode';
-import { AUTH_DETAILS, AUTH_LISTING, GIT_AUTH, GLOBAL_PREFIX } from 'src/configuration';
+import { AUTH_DETAILS, AUTH_LISTING, GLOBAL_PREFIX } from 'src/configuration';
 import { IDataServices } from 'src/repository/data-services.abstract';
 import { ExceptionsService } from 'src/server/exceptions/service';
+import { Permission, Target } from '../permission/entity';
 import { Source } from '../property/entity';
 
 @Injectable()
@@ -18,144 +19,140 @@ export class AuthenticationGuard extends AuthGuard('jwt') {
     super();
   }
 
-  /* @internal
-   * Validating JWT and API Key
-   * Token Format : Bearer {JWT} / Bearer {APIKey}
-   */
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    // @internal no auth for hourSavedAnalyticsBaseURL, added in the login screen
-    if (context.getArgs()[0].url.startsWith(AUTH_LISTING.hourSavedAnalyticsBaseURL)) return true;
     let bearerToken: string;
+    let requesterEmail;
+    let requesterName;
+    let validated: boolean = false;
+    const resource = context.getArgs()[0].route.path.split(GLOBAL_PREFIX)[1];
+    const { method } = context.getArgs()[0];
+    const propertyIdentifier = context.getArgs()[0].body.propertyIdentifier || context.getArgs()[0].params.propertyIdentifier;
+    const secret: string = this.getSecretKey();
+    const options: Object = { secret, algorithms: ['RS256'] };
+
     try {
-      /* eslint-disable prefer-destructuring */
       bearerToken = context.getArgs()[0].headers.authorization.split(' ')[1];
     } catch (err) {
       this.exceptionsService.badRequestException({ message: 'Authentication token missing.' });
     }
+
     try {
-      // @internal Checking that token is JWT or API Key
-      jwt_decode(bearerToken);
+      const payload = jwt_decode(bearerToken);
+      requesterEmail = JSON.parse(JSON.stringify(payload)).email;
+      requesterName = JSON.parse(JSON.stringify(payload)).name;
     } catch (err) {
-      // @internal Validating the API Key
-      const { propertyIdentifier, env } = context.getArgs()[0].params;
-      const url = context.getArgs()[0].url;
-      if (url.startsWith(AUTH_LISTING.deploymentBaseURL)) return this.validateDeploymentRequest(propertyIdentifier, env, bearerToken, context);
-      if (url.startsWith(AUTH_LISTING.eventsBaseURL)) return this.validateEventRequest(propertyIdentifier, bearerToken);
-      if (url.startsWith(AUTH_LISTING.gitDeploymentBaseURL) || url.startsWith(AUTH_LISTING.gitEnvListBaseURL))
-        return this.validateGitRequest(bearerToken);
-      if (context.getArgs()[0].method === 'GET') return this.validateGetRequest(bearerToken);
+      const hashKey = this.getHashKeyForValidation(bearerToken);
+      const { env } = context.getArgs()[0].params;
+      const response = (await this.dataServices.apikey.getByAny({ propertyIdentifier, hashKey }))[0];
+      if (!response) this.exceptionsService.UnauthorizedException({ message: 'Invalid API Key.' });
+      const { expirationDate, createdAt } = response;
+      requesterEmail = response.createdBy;
+
+      if (expirationDate && expirationDate.getTime() <= new Date().getTime())
+        this.exceptionsService.badRequestException({ message: 'API Key is expired.' });
+
+      const deprecatedKey = new Date('2024-08-04');
+      if (createdAt && createdAt.getTime() <= deprecatedKey.getTime()) {
+        context.getArgs()[0].body.createdBy = requesterEmail;
+        if (response.env && response.env.includes(env)) return true;
+        this.exceptionsService.UnauthorizedException({ message: 'Invalid API Key.' });
+        return;
+      }
+      validated = true;
     }
-    const secret: string = this.getSecretKey();
-    const options: Object = { secret, algorithms: ['RS256'] };
-    let validated: boolean = false;
+    context.getArgs()[0].body.createdBy = requesterEmail;
+    context.getArgs()[0].body.creatorName = requesterName;
     try {
-      // @internal Validating the JWT Token
-      validated = Boolean(this.jwtService.verify(bearerToken, options));
+      if (!validated) this.jwtService.verify(bearerToken, options);
     } catch (err) {
       this.exceptionsService.UnauthorizedException(err.message);
     }
+
     try {
-      if (validated) {
-        await this.isAuthorized(bearerToken, context);
-        return true;
-      }
+      await this.isAuthorized(requesterEmail, resource, method, propertyIdentifier, context);
     } catch (err) {
       this.exceptionsService.forbiddenException(err.message);
     }
-    return false;
+    return true;
   }
 
-  private async isAuthorized(bearerToken: string, context: ExecutionContext) {
-    const payload = jwt_decode(bearerToken);
-    const resource = context.getArgs()[0].route.path.split(GLOBAL_PREFIX)[1];
-    const method = context.getArgs()[0].method;
-    const propertyIdentifier = context.getArgs()[0].body.propertyIdentifier || context.getArgs()[0].params.propertyIdentifier;
-    const checkResource = (await this.dataServices.authActionLookup.getByAny({ resource, method }))[0];
-    const email = JSON.parse(JSON.stringify(payload)).email;
-    context.getArgs()[0].body.createdBy = email;
-    if (checkResource) {
-      if (!propertyIdentifier)
-        this.exceptionsService.badRequestException({
-          message: `Please provide the PropertyIdentifier.`
-        });
-      const authorized = (await this.dataServices.permission.getByAny({ propertyIdentifier, email, action: checkResource.name }))[0];
-      if (!authorized)
-        this.exceptionsService.UnauthorizedException({
-          message: `${email} is not authorized to perform this action, please connect with ${propertyIdentifier} owner.`
-        });
+  private async isAuthorized(requesterEmail: string, resource: string, method: string, propertyIdentifier: string, context: ExecutionContext) {
+    const applicationIdentifier = context.getArgs()[0].body.name || context.getArgs()[0].body.applicationIdentifier;
+    const env = context.getArgs()[0].body.env || context.getArgs()[0].params.env;
+    let checkEnv;
+    let cluster;
+    if (env) {
+      checkEnv = (await this.dataServices.environment.getByAny({ propertyIdentifier, env }))[0];
+      cluster = checkEnv.env;
     }
-    // @internal It'll add the created by for the deployment request in the queries, as we're getting the request in the form-body
-    if (context.getArgs()[0].route.path.startsWith(AUTH_LISTING.deploymentBaseURL)) {
+
+    const accessResponse = await this.dataServices.authActionLookup.getByAny({ resource, method });
+    let access;
+
+    if (accessResponse.length === 1) access = accessResponse[0];
+    else if (accessResponse.length === 2 && context.getArgs()[0].route.path.startsWith(AUTH_LISTING.deploymentBaseURL)) {
+      const applicationDetails = await this.dataServices.application.getByAny({
+        identifier: applicationIdentifier,
+        propertyIdentifier
+      });
+
+      if (applicationDetails.length === 0) access = accessResponse.find((obj) => obj.name === 'APPLICATION_CREATION');
+      else access = accessResponse.find((obj) => obj.name === 'APPLICATION_DEPLOYMENT');
       context.getArgs()[0].query.source = Source.MANAGER;
-      context.getArgs()[0].query.createdBy = email;
+      context.getArgs()[0].query.createdBy = requesterEmail;
     }
-    // @internal It'll extract the Name of the Creator specificity for the Property Creation Request
-    if (context.getArgs()[0].route.path === AUTH_LISTING.propertyBaseURL)
-      context.getArgs()[0].body.creatorName = JSON.parse(JSON.stringify(payload)).name;
-  }
 
-  // @internal Request authentication for deployment api
-  private async validateDeploymentRequest(propertyIdentifier: string, env: string, bearerToken: string, context) {
-    if (propertyIdentifier && env) {
-      const hashKey = this.getHashKeyForValidation(bearerToken);
-      const response = (await this.dataServices.apikey.getByAny({ propertyIdentifier, env, hashKey }))[0];
-      if (response) {
-        context.getArgs()[0].query.createdBy = response.createdBy;
-        const { expirationDate } = response;
-        if (expirationDate && expirationDate.getTime() <= new Date().getTime())
-          this.exceptionsService.badRequestException({ message: 'API Key is expired.' });
-        return true;
+    if (!access) return;
+
+    const target = new Target();
+    target.propertyIdentifier = propertyIdentifier;
+    if (access.criteria.includes('cluster')) target.cluster = cluster;
+    if (access.criteria.includes('applicationIdentifier')) target.applicationIdentifier = applicationIdentifier;
+
+    await Object.keys(target).forEach((key) => (target[key] === undefined || target[key] === '') && delete target[key]);
+    const checkAuth = {
+      email: requesterEmail,
+      action: access.name,
+      'target.propertyIdentifier': target.propertyIdentifier,
+      'target.cluster': target.cluster,
+      'target.applicationIdentifier': target.applicationIdentifier
+    };
+    Object.keys(checkAuth).forEach((key) => {
+      if (checkAuth[key] === undefined || checkAuth[key] === '') {
+        delete checkAuth[key];
       }
-      this.exceptionsService.UnauthorizedException({ message: 'Invalid API Key.' });
+    });
+    const authorized = (await this.dataServices.permission.getByAny(checkAuth))[0];
+
+    if (!authorized)
+      this.exceptionsService.forbiddenException({
+        message: `${requesterEmail} is not authorized to perform this action, please connect with ${propertyIdentifier} owner.`
+      });
+
+    if (access.name === 'APPLICATION_CREATION') {
+      const permission = new Permission();
+      permission.name = authorized.name;
+      permission.email = requesterEmail;
+      permission.action = 'APPLICATION_DEPLOYMENT';
+      const target = new Target();
+      target.propertyIdentifier = propertyIdentifier;
+      target.applicationIdentifier = applicationIdentifier;
+      target.cluster = 'preprod';
+      permission.target = target;
+      permission.createdBy = requesterEmail;
+      permission.updatedBy = requesterEmail;
+      await this.dataServices.permission.create(permission);
+      target.cluster = 'prod';
+      await this.dataServices.permission.create(permission);
     }
-    return false;
   }
 
-  // @internal Validate Request for the Git Broker
-  private async validateGitRequest(bearerToken: string) {
-    if (bearerToken === GIT_AUTH.secret) return true;
-    this.exceptionsService.UnauthorizedException({ message: 'Invalid Credentials.' });
-    return false;
-  }
-
-  // @internal Request authentication for events api
-  private async validateEventRequest(propertyIdentifier: string, bearerToken: string) {
-    if (propertyIdentifier) {
-      const hashKey = this.getHashKeyForValidation(bearerToken);
-      const response = (await this.dataServices.apikey.getByAny({ propertyIdentifier, hashKey }))[0];
-      if (response) {
-        const { expirationDate } = response;
-        if (expirationDate && expirationDate.getTime() <= new Date().getTime())
-          this.exceptionsService.badRequestException({ message: 'API Key is expired.' });
-        return true;
-      }
-      this.exceptionsService.UnauthorizedException({ message: 'Invalid API Key.' });
-    }
-    return false;
-  }
-
-  // @internal Request authentication for get api
-  private async validateGetRequest(bearerToken: string) {
-    const hashKey = this.getHashKeyForValidation(bearerToken);
-    const response = (await this.dataServices.apikey.getByAny({ hashKey }))[0];
-    if (response) {
-      const { expirationDate } = response;
-      if (expirationDate && expirationDate.getTime() <= new Date().getTime())
-        this.exceptionsService.badRequestException({ message: 'API Key is expired.' });
-      return true;
-    }
-    this.exceptionsService.UnauthorizedException({ message: 'Invalid API Key.' });
-    return false;
-  }
-
-  // @internal Get the Secret key for the JWT Validation
   private getSecretKey(): string {
     const publicKey = AUTH_DETAILS.pubkey;
     if (!publicKey || publicKey.trim().length === 0) this.exceptionsService.internalServerErrorException({ message: 'Public Key not found.' });
     return this.formatAsPem(publicKey);
   }
 
-  // @internal Construction of PEM for the JWT Secret
   private formatAsPem(key: string): string {
     const keyHeader = '-----BEGIN PUBLIC KEY-----';
     const keyFooter = '-----END PUBLIC KEY-----';
