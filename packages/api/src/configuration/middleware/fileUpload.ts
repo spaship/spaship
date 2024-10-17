d697422dd779eadab3b394a4b539e9e2a8a1e8a4
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2024-10-17 14:56:01 +0530

    feat(api) : fileupload middleware added

---
+++ packages/api/src/configuration/middleware/fileUpload.ts
@@ -0,0 +1,32 @@
+
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import * as multer from 'multer';
+import { DIRECTORY_CONFIGURATION, VALIDATION } from 'src/Configuration';
+
+@Injectable()
+export class FileUploadMiddleware implements NestMiddleware {
+  use(req: any, res: any, next: () => void) {
+    const storage = multer.diskStorage({
+      destination: DIRECTORY_CONFIGURATION.baseDir,
+      filename: (req, file, callback) => {
+        const filename = `${Date.now()}-${file.originalname.replace(VALIDATION.FILE, '_')}`;
+        callback(null, filename);
+      }
+    });
+
+    const upload = multer({
+      storage,
+      fileFilter: (req, file, cb) => {
+        file.filename = `${Date.now()}-${file.originalname.replace(VALIDATION.FILE, '_')}`;
+        cb(null, true);
+      }
+    }).single('upload');
+
+    upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).send({ message: err.message });
+      }
+      next();
+    });
+  }
+}
